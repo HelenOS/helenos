b312247faf4fc145c5035a5a7199b78d9a33cda7
Jakub Jermar <jakub@jermar.eu>
2005-05-19 13:16:34 +0000

    Add BSD license and copyright line. Coding style and small improvements.

--- test/fp/fp0/test.c
+++ test/fp/fp0/test.c
@@ -1,5 +1,30 @@
-
-
+/*
+ * Copyright (C) 2005 Jakub Vana
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <arch/interrupt.h>
 #include <print.h>
@@ -11,60 +36,38 @@
 #include <arch/asm.h>
 #include <mm/tlb.h>
 
-
-
 #include <test.h>
 #include <arch.h>
 #include <arch/smp/atomic.h>
 #include <proc/thread.h>
 
-
-
-
-thread_t *thread_create(void (* func)(void *), void *arg, task_t *task, int flags);
-
-
-static void thread(void *data)
+static void e(void *data)
 {
-  while(1) 
-  {
-    double e,d,le,f;
-    le=-1;
-    e=0;
-    f=1;
-    for(d=1;e!=le;d*=f,f+=1) {le=e;e=e+1/d;}
+	while(1) {
+		double e,d,le,f;
+		le=-1;
+		e=0;
+		f=1;
+		for(d=1;e!=le;d*=f,f+=1) {
+			le=e;
+			e=e+1/d;
+		}
     
-    if((int)(100000000*e)==271828182) printf("THREAD:%s e OK\n",(char*)data);
-    else panic("THREAD:%s e Failed\n",(char*)data);
-
-    
-//    printf("100000000*e:%d\n");
-  }
-  //printf("TEST:%s\n",(char*)data);  
+		if((int)(100000000*e)==271828182) printf("tid%d: e OK\n", THREAD->tid);
+		else panic("tid%d: e FAILED (100000000*e=%d)\n", THREAD->tid, (int) 100000000*e);
+	}
 }
 
 
 
 void test(void)
 {
-  thread_t *t;
-  
-  t=thread_create(thread, (void*)"0", TASK,0);
-  thread_ready(t);
+	thread_t *t;
+	int i;
 
-
-
-  t=thread_create(thread, (void*)"1", TASK,0);
-  thread_ready(t);
-
-
-
-  t=thread_create(thread, (void*)"2", TASK,0);
-  thread_ready(t);
-
-  t=thread_create(thread, (void*)"3", TASK,0);
-  thread_ready(t);
-  while(1);
+	for (i=0; i<4; i++) {  
+		t = thread_create(e, NULL, TASK, 0);
+		thread_ready(t);
+	}
 
 }
-
