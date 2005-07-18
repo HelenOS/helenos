b4a4c5e34cd7923c48b3a276959211a168f0f37c
Martin Decky <martin@decky.cz>
2005-07-18 09:27:16 +0000

    Start ASID manipulation

---
+++ arch/mips/src/mm/asid.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2005 HelenOS Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <arch.h>
+#include <memstr.h>
+
+/**< Array of threads that have currently some ASID assigned,
+     NULL means no thread have ASID with number of that index assigned */
+struct thread * asids[256];
+int last_asid; /**< The number of last assigned ASID */
+int asid_bitmap[32]; /**< Bitmap of ASIDs currently in TLB */
+
+
+/** Cleanup asid_bitmap
+ *
+ */
+void asid_bitmap_reset()
+{
+	memsetb(asid_bitmap, sizeof(asid_bitmap), 0);
+}
+
+
+/** Initialize manipulating with ASIDs
+ *
+ */
+void init_asids()
+{
+	memsetb(asids, sizeof(asids), 0);
+	asid_bitmap_reset();
+	last_asid = 0;
+}
