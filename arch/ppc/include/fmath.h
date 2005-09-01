c7bb8b05da36424f3a544f20383a616e25ec8633
Josef Cejka <malyzelenyhnus@seznam.cz>
2005-09-01 15:26:27 +0000

    Created fmath as basic tool for IEEE 754 floating point  numbers. Added option $F for printf for printing "double" type.

---
+++ arch/ppc/include/fmath.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2005 Josef Cejka
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */ 
+
+#include <arch/types.h>
+ 
+#define FMATH_EXPONENT_BIAS 1023
+
+typedef unsigned char fmath_ld_descr_t[8];
+ 
+typedef union { double bf; unsigned char ldd[8]; }  fmath_ld_union_t;
+
+int fmath_is_negative(double num);
+//int fmath_is_exponent_negative(double num);
+
+/**returns exponent in binary encoding*/
+signed short fmath_get_binary_exponent(double num);
+
+/**returns exponent in decimal encoding*/
+double fmath_get_decimal_exponent(double num);
+
+/**returns mantisa in binary encoding */
+__u64 fmath_get_binary_mantisa(double num) ;
+
+/** Function for extract integer part from double
+* @param num input value
+* @param intp integer part of num
+* @return non-integer part
+*/
+double fmath_fint(double num, double *intp);
+
+/** Return absolute value from num */
+double fmath_abs(double num);
+
+double fmath_set_sign(double num,__u8 sign);
+
+/** count base^exponent from positive exponent
+* @param base
+* @param exponent - Must be > 0.0 
+* @return base^exponent or 0.0 (if exponent <=0.0)
+*/
+double fmath_dpow(double base, double exponent) ;
+
---
+++ arch/ppc/src/fmath.c
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2005 Josef Cejka
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */ 
+
+#include <arch/fmath.h>
+#include <print.h>
+
+	//TODO: 
+#define FMATH_MANTISA_MASK ( 0x000fffffffffffffLL )
+
+int fmath_is_negative(double num) 
+{	//TODO:
+/*	fmath_ld_union_t fmath_ld_union;
+	fmath_ld_union.bf = num;
+	return ((fmath_ld_union.ldd[7])&0x80)==0x80; //first bit is sign, IA32 is little endian -> 8th byte
+*/
+	return 0;
+} 
+
+signed short fmath_get_binary_exponent(double num) 
+{	//TODO:
+/*	fmath_ld_union_t fmath_ld_union;
+	fmath_ld_union.bf = num;
+	return (signed short)((((fmath_ld_union.ldd[7])&0x7f)<<4) + (((fmath_ld_union.ldd[6])&0xf0)>>4)) -FMATH_EXPONENT_BIAS; // exponent is 11 bits lenght, so sevent bits is in 8th byte and 4 bits in 7th 
+*/
+	return 0;
+}
+
+double fmath_get_decimal_exponent(double num) 
+{	//TODO:
+	double value;
+	// log10(2)*log2(x) => log10(x) 
+/*	__asm__ __volatile__ ( \
+	"fldlg2		#load log10(2)	\n\t"	\
+	"fxch %%st(1) 		\n\t" \
+	"fyl2x		#count st(0)*log2(st(1))->st(1); pop st(0)	\n\t" \
+	: "=t" (value) : "0"(num) );
+*/	return value;
+	
+}
+
+__u64 fmath_get_binary_mantisa(double num) 
+{	//TODO:
+/*	union { __u64 _u; double _d;} un = { _d : num };
+	un._u=un._u &(FMATH_MANTISA_MASK); // mask 52 bits of mantisa
+	return un._u;
+	*/
+	return 0;
+}
+
+double fmath_fint(double num, double *intp) 
+{	//TODO:
+/*	fmath_ld_union_t fmath_ld_union_num;
+	fmath_ld_union_t fmath_ld_union_int;
+	signed short exp;
+	__u64 mask,mantisa;
+	int i;
+	
+	exp=fmath_get_binary_exponent(num);
+	
+	if (exp<0) {
+		*intp = 0.0;
+		*intp = fmath_set_sign(0.0L,fmath_is_negative(num));
+		return num;
+		}
+		
+
+	if (exp>51) {
+		*intp=num;
+		num=0.0;
+		num= fmath_set_sign(0.0L,fmath_is_negative(*intp));
+		return num;
+	}
+	
+	fmath_ld_union_num.bf = num;
+	
+	mask = FMATH_MANTISA_MASK>>exp;
+	//mantisa = (fmath_get-binary_mantisa(num))&(~mask);
+	
+	for (i=0;i<7;i++) {
+		// Ugly construction for obtain sign, exponent and integer part from num 
+		fmath_ld_union_int.ldd[i]=fmath_ld_union_num.ldd[i]&(((~mask)>>(i*8))&0xff);
+	}
+	
+	fmath_ld_union_int.ldd[6]|=((fmath_ld_union_num.ldd[6])&(0xf0));
+	fmath_ld_union_int.ldd[7]=fmath_ld_union_num.ldd[7];
+	
+	*intp=fmath_ld_union_int.bf;
+	return fmath_ld_union_num.bf-fmath_ld_union_int.bf;
+*/
+	
+	return 0.0;
+};
+	
+double fmath_set_sign(double num,__u8 sign) 
+{	//TODO:
+/*	fmath_ld_union_t fmath_ld_union;
+	fmath_ld_union.bf = num;
+	fmath_ld_union.ldd[7]=((fmath_ld_union.ldd[7])&0x7f)|(sign<<7); // change 64th bit (IA32 is a little endian)
+	return fmath_ld_union.bf;
+*/	return 1.0
+}
+
+double fmath_abs(double num) 
+{	//TODO:
+/*
+	return fmath_set_sign(num,0);
+*/
+	return 1.0;
+}
+
+double fmath_dpow(double base, double exponent) 
+{	//TODO:
+/*	double value=1.0;
+	if (base<=0.0) return base; 
+	
+	//2^(x*log2(10)) = 2^y = 10^x
+	
+	__asm__ __volatile__ (		\
+		"fyl2x # ST(1):=ST(1)*log2(ST(0)), pop st(0) \n\t "		\
+		"fld	%%st(0) \n\t"	\
+		"frndint \n\t"		\
+		"fxch %%st(1) \n\t"		\
+		"fsub %%st(1),%%st(0) \n\t"	\
+		"f2xm1	# ST := 2^ST -1\n\t"			\
+		"fld1 \n\t"			\
+		"faddp %%st(0),%%st(1) \n\t"	\
+		"fscale #ST:=ST*2^(ST(1))\n\t"		\
+		"fstp %%st(1) \n\t"		\
+	"" : "=t" (value) :  "0" (base), "u" (exponent) );
+	return value;
+*/
+	return 1.0;
+}
+
