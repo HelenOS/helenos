da7d77077456ab35a65d42080d3cfbd4dafaa60a
Jakub Vana <jakub.vana@gmail.com>
2005-09-02 18:29:28 +0000

    Serialization of psr write

--- arch/ia64/src/cpu/cpu.c
+++ arch/ia64/src/cpu/cpu.c
@@ -14,25 +14,28 @@ void cpu_arch_init(void)
 
 
     int *p=&IVT;
-    
     volatile __u64 hlp,hlp2;
 
 
     int psr = 0x2000;
+    
 
 	__asm__  volatile (
-		"mov r15 = %0;;"
-		"mov cr2 = r15;;"
-		"mov psr.l = %1;;"
+		"mov cr2 = %0;;\n"
+		"mov psr.l = %1;;\n"
+		"srlz.i;"
+		"srlz.d;;"
 		: 
 		: "r" (p), "r" (psr)
-		: "r15"
 	);
 
 
 
 	/*Switch register bank of regs r16 .. r31 to 1 It is automaticly cleared on exception*/
-	__asm__ volatile ("bsw.1;;");             
+	__asm__ volatile 
+	(
+	    "bsw.1;;\n"
+	);             
 	
 
 }
