2c457e89e009478a9eb0fb1e49cb0128fbedb3bf
Martin Decky <martin@decky.cz>
2005-11-05 21:43:29 +0000

    update comments

--- arch/ia32/src/smp/apic.c
+++ arch/ia32/src/smp/apic.c
@@ -46,6 +46,8 @@
  *	Bochs 2.0.2 - Bochs 2.2 with 2-8 CPUs
  *	Simics 2.0.28 - Simics 2.2.14 2-4 CPUs
  *	ASUS P/I-P65UP5 + ASUS C-P55T2D REV. 1.41 with 2x 200Mhz Pentium CPUs
+ *	ASUS PCH-DL with 2x 3000Mhz Pentium 4 Xeon (HT) CPUs
+ *	MSI K7D Master-L with 2x 2100MHz Athlon MP CPUs
  */
 
 /*
