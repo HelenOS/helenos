4a28943658bd8e80c365754d9d3a95c56c582b17
Jakub Vana <jakub.vana@gmail.com>
2005-11-27 21:40:25 +0000

    Lost files from last commit.

---
+++ arch/ia64/src/drivers/keyboard.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2003 Josef Cejka
+ * Copyright (C) 2005 Jakub Jermar
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <arch/drivers/keyboard.h>
+#include <console/chardev.h>
+#include <console/console.h>
+#include <putchar.h>
+#include <synch/spinlock.h>
+#include <synch/waitq.h>
+#include <typedefs.h>
+#include <print.h>
+
+static void keyboard_enable(void);
+static void keyboard_disable(void);
+
+static chardev_t kbrd;
+static chardev_operations_t ops = {
+	.resume = keyboard_enable,
+	.suspend = keyboard_disable
+};
+
+
+int kb_disable;
+
+
+/** Initialize keyboard subsystem. */
+void keyboard_init(void)
+{
+	chardev_initialize(&kbrd, &ops);
+	stdin = &kbrd;
+	kb_disable = 0;
+}
+
+/** Process keyboard interrupt. */
+void keyboard(void)
+{
+	if(kb_disable) return;
+	char ch;
+
+	ch = ski_getchar();
+	if(ch==13) ch=10; 
+	if(ch) chardev_push_character(&kbrd, ch);
+	
+}
+
+/* Called from getc(). */
+void keyboard_enable(void)
+{
+	kb_disable=0;
+}
+
+/* Called from getc(). */
+void keyboard_disable(void)
+{
+	kb_disable=1;	
+}
