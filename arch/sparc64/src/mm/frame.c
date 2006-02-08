85dc2e77222d3673eee1c67e4bf6f866fda4ca31
Jakub Jermar <jakub@jermar.eu>
2006-02-08 22:35:22 +0000

    Do not add CONFIG_STACK_SIZE to kernel.size.

--- arch/sparc64/src/mm/frame.c
+++ arch/sparc64/src/mm/frame.c
@@ -33,7 +33,7 @@
 
 void frame_arch_init(void)
 {
-	zone_create(0, config.memory_size >> FRAME_WIDTH, ADDR2PFN(ALIGN_UP(config.base + config.kernel_size + CONFIG_STACK_SIZE, FRAME_SIZE)), 0);
+	zone_create(0, config.memory_size >> FRAME_WIDTH, ADDR2PFN(ALIGN_UP(config.base + config.kernel_size, FRAME_SIZE)), 0);
 
 	/*
 	 * Workaround to prevent slab allocator from allocating frame 0.
