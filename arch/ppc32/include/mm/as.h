096787721c478388c17f5363b2556186808863fe
Martin Decky <martin@decky.cz>
2006-03-13 15:20:32 +0000

    ppc32 boots again (temporarily)

--- arch/ppc32/include/mm/as.h
+++ arch/ppc32/include/mm/as.h
@@ -31,7 +31,7 @@
 
 #include <arch/types.h>
 
-#define KERNEL_ADDRESS_SPACE_START_ARCH		(__address) 0x80000000
+#define KERNEL_ADDRESS_SPACE_START_ARCH		(__address) 0x00000000
 #define KERNEL_ADDRESS_SPACE_END_ARCH		(__address) 0xffffffff	
 #define USER_ADDRESS_SPACE_START_ARCH		(__address) 0x00000000
 #define USER_ADDRESS_SPACE_END_ARCH		(__address) 0x7fffffff
