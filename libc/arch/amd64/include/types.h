18acad554ff6cb14b78fd376e87bd4ce4724c792
Josef Cejka <malyzelenyhnus@seznam.cz>
2006-03-14 13:03:35 +0000

    Added missing uspace types definitions to non-ia32 arch.

--- libc/arch/amd64/include/types.h
+++ libc/arch/amd64/include/types.h
@@ -33,4 +33,14 @@ typedef unsigned long long sysarg_t;
 typedef unsigned int size_t;
 typedef signed int ssize_t;
 
+typedef char int8_t;
+typedef short int int16_t;
+typedef int int32_t;
+typedef long long int int64_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long int uint64_t;
+
 #endif
--- libc/arch/mips32/include/types.h
+++ libc/arch/mips32/include/types.h
@@ -33,4 +33,14 @@ typedef unsigned int sysarg_t;
 typedef unsigned int size_t;
 typedef signed int ssize_t;
 
+typedef char int8_t;
+typedef short int int16_t;
+typedef long int int32_t;
+typedef long long int int64_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned long int uint32_t;
+typedef unsigned long long int uint64_t;
+
 #endif
