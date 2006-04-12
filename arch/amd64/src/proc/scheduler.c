a597e3f03d1d6dfa29bde4a7c7dfb4a1b2ecc0e6
Ondrej Palkovsky <ondrap@penguin.cz>
2006-04-12 09:55:15 +0000

    Comment update.

--- arch/amd64/src/proc/scheduler.c
+++ arch/amd64/src/proc/scheduler.c
@@ -39,7 +39,8 @@ void before_thread_runs_arch(void)
 {
 	CPU->arch.tss->rsp0 = (__address) &THREAD->kstack[THREAD_STACK_SIZE-SP_DELTA];
 
-	/* Syscall support - write thread stack address to hidden part of gs */
+	/* Syscall support - write address of thread stack pointer to 
+	 * hidden part of gs */
 	swapgs();
 	write_msr(AMD_MSR_GS,
 		  (__u64)&THREAD->kstack);
