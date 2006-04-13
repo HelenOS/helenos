0ddeabc57b1d75f43de34a8874ed2d9af0416f7e
Jakub Jermar <jakub@jermar.eu>
2006-04-13 17:46:52 +0000

    Add important comment.

--- arch/ia32/src/pm.c
+++ arch/ia32/src/pm.c
@@ -65,6 +65,7 @@ descriptor_t gdt[GDT_ITEMS] = {
 	{ 0xffff, 0, 0, AR_PRESENT | AR_DATA | AR_WRITABLE | DPL_USER, 0xf, 0, 0, 1, 1, 0 },
 	/* TSS descriptor - set up will be completed later */
 	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	/* TLS descriptor */
 	{ 0xffff, 0, 0, AR_PRESENT | AR_DATA | AR_WRITABLE | DPL_USER, 0xf, 0, 0, 1, 1, 0 }
 };
 
