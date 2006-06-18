07cb1b59fad9331c6b03394ab28eff1ea88a7061
Martin Decky <martin@decky.cz>
2006-06-18 22:52:07 +0000

    get back -X removed by mistake

--- contrib/conf/gxemul.sh
+++ contrib/conf/gxemul.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Uspace addresses outside of normal memory (kernel has std. 8 or 16MB)
 # we place the pages at 24M
-	gxemul $@ -E testmips image.boot 
+	gxemul $@ -E testmips -X image.boot 
