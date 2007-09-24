56bc4a359147ae6490817d6fd0f5d88f628bebbd
Jakub Vana <jakub.vana@gmail.com>
2007-09-24 10:06:29 +0000

    IA64 keyboard lost headerfile

---
+++ kernel/arch/ia64/include/drivers/i8042.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2006 Jakub Jermar
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @addtogroup ia32	
+ * @{
+ */
+/** @file
+ */
+
+/**
+ * This file implements ia32 specific access to i8042 registers.
+ */
+
+#ifndef KERN_ia32_I8042_H_
+#define KERN_ia32_I8042_H_
+
+#include <arch/asm.h>
+#include <arch/types.h>
+
+#define i8042_DATA		0x60
+#define i8042_STATUS		0x64
+
+static inline void i8042_data_write(uint8_t data)
+{
+	outb(i8042_DATA, data);
+}
+
+static inline uint8_t i8042_data_read(void)
+{
+	return inb(i8042_DATA);
+}
+
+static inline uint8_t i8042_status_read(void)
+{
+	return inb(i8042_STATUS);
+}
+
+static inline void i8042_command_write(uint8_t command)
+{
+	outb(i8042_STATUS, command);
+}
+
+#endif
+
+/** @}
+ */
