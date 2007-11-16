454889cf970b9dafbdd70948d1bcb1a779908dcc
Jakub Jermar <jakub@jermar.eu>
2007-11-16 16:22:11 +0000

    Support for six syscall arguments for ppc32.

--- uspace/lib/libc/arch/ppc32/src/syscall.c
+++ uspace/lib/libc/arch/ppc32/src/syscall.c
@@ -36,13 +36,16 @@
 
 #include <libc.h>
 
-sysarg_t __syscall(const sysarg_t p1, const sysarg_t p2, const sysarg_t p3, const sysarg_t p4, const syscall_t id)
+sysarg_t __syscall(const sysarg_t p1, const sysarg_t p2, const sysarg_t p3,
+    const sysarg_t p4, const sysarg_t p5, const sysarg_t p6, const syscall_t id)
 {
 	register sysarg_t __ppc32_reg_r3 asm("3") = p1;
 	register sysarg_t __ppc32_reg_r4 asm("4") = p2;
 	register sysarg_t __ppc32_reg_r5 asm("5") = p3;
 	register sysarg_t __ppc32_reg_r6 asm("6") = p4;
-	register sysarg_t __ppc32_reg_r7 asm("7") = id;
+	register sysarg_t __ppc32_reg_r7 asm("7") = p5;
+	register sysarg_t __ppc32_reg_r8 asm("8") = p6;
+	register sysarg_t __ppc32_reg_r9 asm("9") = id;
 	
 	asm volatile (
 		"sc\n"
@@ -51,7 +54,9 @@ sysarg_t __syscall(const sysarg_t p1, const sysarg_t p2, const sysarg_t p3, cons
 		  "r" (__ppc32_reg_r4),
 		  "r" (__ppc32_reg_r5),
 		  "r" (__ppc32_reg_r6),
-		  "r" (__ppc32_reg_r7)
+		  "r" (__ppc32_reg_r7),
+		  "r" (__ppc32_reg_r8),
+		  "r" (__ppc32_reg_r9)
 	);
 	
 	return __ppc32_reg_r3;
