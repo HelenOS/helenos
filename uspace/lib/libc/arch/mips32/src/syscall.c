9c2fb975fdc54c332e4b3e32c14e0f14909324b3
Jakub Jermar <jakub@jermar.eu>
2007-11-16 16:18:29 +0000

    Support for six syscall arguments on mips32.

--- uspace/lib/libc/arch/mips32/src/syscall.c
+++ uspace/lib/libc/arch/mips32/src/syscall.c
@@ -35,14 +35,15 @@
 
 #include <libc.h>
 
-sysarg_t __syscall(const sysarg_t p1, const sysarg_t p2, 
-		   const sysarg_t p3, const sysarg_t p4,
-		   const syscall_t id)
+sysarg_t __syscall(const sysarg_t p1, const sysarg_t p2, const sysarg_t p3,
+    const sysarg_t p4, const sysarg_t p5, const sysarg_t p6, const syscall_t id)
 {
 	register sysarg_t __mips_reg_a0 asm("$4") = p1;
 	register sysarg_t __mips_reg_a1 asm("$5") = p2;
 	register sysarg_t __mips_reg_a2 asm("$6") = p3;
 	register sysarg_t __mips_reg_a3 asm("$7") = p4;
+	register sysarg_t __mips_reg_t0 asm("$8") = p5;
+	register sysarg_t __mips_reg_t1 asm("$9") = p6;
 	register sysarg_t __mips_reg_v0 asm("$2") = id;
 	
 	asm volatile (
@@ -52,6 +53,8 @@ sysarg_t __syscall(const sysarg_t p1, const sysarg_t p2,
 		  "r" (__mips_reg_a1),
 		  "r" (__mips_reg_a2),
 		  "r" (__mips_reg_a3),
+		  "r" (__mips_reg_t0),
+		  "r" (__mips_reg_t1),
 		  "r" (__mips_reg_v0)
 		: "%ra" /* We are a function call, although C does not 
 			 * know it */
