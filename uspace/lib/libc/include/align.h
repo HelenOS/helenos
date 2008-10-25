71f1cc1dd43b4489b3635e4b8c512aa23359d9b3
Jakub Jermar <jakub@jermar.eu>
2008-10-25 14:58:54 +0000

    Add the ROUND_UP() macro.

--- uspace/lib/libc/include/align.h
+++ uspace/lib/libc/include/align.h
@@ -35,20 +35,27 @@
 #ifndef LIBC_ALIGN_H_
 #define LIBC_ALIGN_H_
 
-/** Align to the nearest lower address.
+/** Align to the nearest lower address which is a power of two.
  *
- * @param s Address or size to be aligned.
- * @param a Size of alignment, must be power of 2.
+ * @param s		Address or size to be aligned.
+ * @param a		Size of alignment, must be power of 2.
  */
 #define ALIGN_DOWN(s, a)	((s) & ~((a) - 1))
 
 
-/** Align to the nearest higher address.
+/** Align to the nearest higher address which is a power of two.
  *
- * @param s Address or size to be aligned.
- * @param a Size of alignment, must be power of 2.
+ * @param s		Address or size to be aligned.
+ * @param a		Size of alignment, must be power of 2.
  */
 #define ALIGN_UP(s, a)		((long)((s) + ((a) - 1)) & ~((long) (a) - 1))
+
+/** Round up to the nearest higher boundary.
+ *
+ * @param n		Number to be aligned.
+ * @param b		Boundary, arbitrary unsigned number.
+ */
+#define ROUND_UP(n, b)		(((n) / (b) + ((n) % (b) != 0)) * (b))
 
 #endif
 
