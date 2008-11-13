37e518bcb08eaccfcdca2bc0e9eca06f4ebc26d2
Jiri Svoboda <jirik.svoboda@seznam.cz>
2008-11-13 10:37:18 +0000

    Bugfix: A ppc32 task would hang if it touched unmapped memory - was calling printf() with address-space mutex held.

--- kernel/arch/ppc32/src/mm/tlb.c
+++ kernel/arch/ppc32/src/mm/tlb.c
@@ -95,8 +95,8 @@ find_mapping_and_check(as_t *as, bool lock, uintptr_t badvaddr, int access,
 			*pfrc = rc;
 			return NULL;
 		case AS_PF_FAULT:
-			page_table_lock(as, lock);
 			printf("Page fault.\n");
+			page_table_lock(as, lock);
 			*pfrc = rc;
 			return NULL;
 		default:
