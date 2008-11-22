ce4a3dae093e94753a7db2c045dd2a93e9bf578f
Jiri Svoboda <jirik.svoboda@seznam.cz>
2008-11-22 10:35:34 +0000

    Add endless loop test.

--- uspace/app/tester/loop/loop1.c
+++ uspace/app/tester/loop/loop1.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 Martin Decky
+ * Copyright (c) 2008 Jiri Svoboda
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,55 +26,16 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/** @addtogroup tester
- * @{
- */
-/** @file
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include "../tester.h"
 
-#ifndef TESTER_H_
-#define TESTER_H_
+char *test_loop1(bool quiet)
+{
+	printf("Looping...\n");
+	while (1);
+	printf("Survived endless loop?!!\n");
 
-#include <sys/types.h>
-#include <bool.h>
-#include <ipc/ipc.h>
+	return NULL;
+}
 
-#define IPC_TEST_START	10000
-#define MAX_PHONES		20
-#define MAX_CONNECTIONS 50
-#define TEST_SKIPPED    "Test Skipped"
-
-extern int myservice;
-extern int phones[MAX_PHONES];
-extern int connections[MAX_CONNECTIONS];
-extern ipc_callid_t callids[MAX_CONNECTIONS];
-
-typedef char * (* test_entry_t)(bool);
-
-typedef struct {
-	char * name;
-	char * desc;
-	test_entry_t entry;
-	bool safe;
-} test_t;
-
-extern char * test_thread1(bool quiet);
-extern char * test_print1(bool quiet);
-extern char * test_fault1(bool quiet);
-extern char * test_fault2(bool quiet);
-extern char * test_register(bool quiet);
-extern char * test_connect(bool quiet);
-extern char * test_send_async(bool quiet);
-extern char * test_send_sync(bool quiet);
-extern char * test_answer(bool quiet);
-extern char * test_hangup(bool quiet);
-extern char * test_devmap1(bool quiet);
-extern char * test_loop1(bool quiet);
-extern char * test_vfs1(bool quiet);
-
-extern test_t tests[];
-
-#endif
-
-/** @}
- */
