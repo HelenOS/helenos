04de76596e4d05a3dc73b8214d009de852888337
Martin Decky <martin@decky.cz>
2008-12-17 15:28:35 +0000

    update to latest versions

--- contrib/toolchain/toolchain.sparc64.sh
+++ contrib/toolchain/toolchain.sparc64.sh
@@ -19,8 +19,8 @@ if [ -z "${CROSS_PREFIX}" ] ; then
     CROSS_PREFIX="/usr/local"
 fi
 
-BINUTILS_VERSION="2.18"
-GCC_VERSION="4.3.1"
+BINUTILS_VERSION="2.19"
+GCC_VERSION="4.3.2"
 
 BINUTILS="binutils-${BINUTILS_VERSION}.tar.gz"
 GCC_CORE="gcc-core-${GCC_VERSION}.tar.bz2"
