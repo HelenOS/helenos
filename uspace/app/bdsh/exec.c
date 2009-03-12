29295cdb66dfe98fbf408425f03b9a8569e435c5
Jiri Svoboda <jirik.svoboda@seznam.cz>
2009-03-12 19:18:23 +0000

    Fix grammar.

--- uspace/app/bdsh/exec.c
+++ uspace/app/bdsh/exec.c
@@ -121,7 +121,7 @@ unsigned int try_exec(char *cmd, char **argv)
 	free(tmp);
 
 	if (tid == 0) {
-		cli_error(CL_EEXEC, "Can not spawn %s", cmd);
+		cli_error(CL_EEXEC, "Cannot spawn `%s'.", cmd);
 		return 1;
 	} else {
 		return 0;
