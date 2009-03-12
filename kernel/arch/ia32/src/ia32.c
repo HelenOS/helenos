2a34e4c88f99ff600ede84d45f559c28c65e2ea5
Martin Decky <martin@decky.cz>
2009-03-12 18:00:23 +0000

    update for the new scheme of wiring device drivers to keyboard/serial modules use ega_redraw() only if it is compiled in

--- kernel/arch/ia32/src/ia32.c
+++ kernel/arch/ia32/src/ia32.c
@@ -113,7 +113,7 @@ void arch_post_mm_init(void)
 		i8254_init();
 		
 #ifdef CONFIG_FB
-		if (vesa_present()) 
+		if (vesa_present())
 			vesa_init();
 		else
 #endif
@@ -151,17 +151,19 @@ void arch_pre_smp_init(void)
 
 void arch_post_smp_init(void)
 {
+#ifdef CONFIG_PC_KBD
 	devno_t devno = device_assign_devno();
-
+	
 	/*
-	 * Initialize the keyboard module and conect it to stdin. Then
-	 * initialize the i8042 controller and connect it to kbrdin. Enable
-	 * keyboard interrupts.
+	 * Initialize the i8042 controller. Then initialize the keyboard
+	 * module and connect it to i8042. Enable keyboard interrupts.
 	 */
-	kbrd_init(stdin);
-	(void) i8042_init((i8042_t *) I8042_BASE, devno, IRQ_KBD, &kbrdin);
-	trap_virtual_enable_irqs(1 << IRQ_KBD);
-
+	indev_t *kbrdin = i8042_init((i8042_t *) I8042_BASE, devno, IRQ_KBD);
+	if (kbrdin) {
+		kbrd_init(kbrdin);
+		trap_virtual_enable_irqs(1 << IRQ_KBD);
+	}
+	
 	/*
 	 * This is the necessary evil until the userspace driver is entirely
 	 * self-sufficient.
@@ -173,6 +175,7 @@ void arch_post_smp_init(void)
 	    (uintptr_t) I8042_BASE);
 	sysinfo_set_item_val("kbd.address.kernel", NULL,
 	    (uintptr_t) I8042_BASE);
+#endif
 }
 
 void calibrate_delay_loop(void)
@@ -206,9 +209,14 @@ unative_t sys_tls_set(unative_t addr)
 void arch_grab_console(void)
 {
 #ifdef CONFIG_FB
-	vesa_redraw();
+	if (vesa_present())
+		vesa_redraw();
+	else
+#endif
+#ifdef CONFIG_EGA
+		ega_redraw();
 #else
-	ega_redraw();
+		{}
 #endif
 }
 
