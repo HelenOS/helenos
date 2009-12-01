4af185f6b79c7584baf90bdf41a19cdf11f6c69e
Jakub Jermar <jakub@jermar.eu>
2009-12-01 21:46:23 +0100

    Clean up futex.h.

--- kernel/generic/include/synch/futex.h
+++ kernel/generic/include/synch/futex.h
@@ -37,8 +37,6 @@
 
 #include <arch/types.h>
 #include <synch/waitq.h>
-#include <genarch/mm/page_ht.h>
-#include <genarch/mm/page_pt.h>
 
 /** Kernel-side futex structure. */
 typedef struct {
@@ -53,9 +51,8 @@ typedef struct {
 } futex_t;
 
 extern void futex_init(void);
-extern unative_t sys_futex_sleep_timeout(uintptr_t uaddr, uint32_t usec,
-    int flags);
-extern unative_t sys_futex_wakeup(uintptr_t uaddr);
+extern unative_t sys_futex_sleep_timeout(uintptr_t, uint32_t, int);
+extern unative_t sys_futex_wakeup(uintptr_t);
 
 extern void futex_cleanup(void);
 
