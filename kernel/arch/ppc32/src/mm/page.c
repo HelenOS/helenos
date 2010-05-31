6e2bf292018603bfc698dec43b84ec2c0ede62f8
Jakub Jermar <jakub@jermar.eu>
2010-05-31 21:09:07 +0200

    Lock the page tables before working with them on ppc32.

--- kernel/arch/ppc32/src/mm/page.c
+++ kernel/arch/ppc32/src/mm/page.c
@@ -53,9 +53,11 @@ uintptr_t hw_map(uintptr_t physaddr, size_t size)
 	
 	uintptr_t virtaddr = PA2KA(last_frame);
 	pfn_t i;
+	page_table_lock(AS_KERNEL, true);
 	for (i = 0; i < ADDR2PFN(ALIGN_UP(size, PAGE_SIZE)); i++)
 		page_mapping_insert(AS_KERNEL, virtaddr + PFN2ADDR(i),
 		    physaddr + PFN2ADDR(i), PAGE_NOT_CACHEABLE | PAGE_WRITE);
+	page_table_unlock(AS_KERNEL, true);
 	
 	last_frame = ALIGN_UP(last_frame + size, FRAME_SIZE);
 	
