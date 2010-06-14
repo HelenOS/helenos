e5c1186d6bb5d82ffce0d76cda79942647f1536e
Jakub Jermar <jakub@jermar.eu>
2010-06-15 00:58:20 +0200

    Remove the poor man's support for SMP.
    Proper SMP support will be reintroduced later after the rest of the ia64 code is
    cleaned up and the real hardware support is back.

--- kernel/arch/ia64/src/smp/smp.c
+++ kernel/arch/ia64/src/smp/smp.c
@@ -32,132 +32,21 @@
 /** @file
  */
 
-#include <arch.h>
-#include <arch/drivers/ski.h>
-#include <arch/drivers/it.h>
-#include <arch/interrupt.h>
-#include <arch/barrier.h>
-#include <arch/asm.h>
-#include <arch/register.h>
-#include <typedefs.h>
-#include <arch/context.h>
-#include <arch/stack.h>
-#include <arch/mm/page.h>
-#include <mm/as.h>
-#include <config.h>
-#include <userspace.h>
-#include <console/console.h>
-#include <proc/uarg.h>
-#include <syscall/syscall.h>
-#include <ddi/irq.h>
-#include <ddi/device.h>
-#include <arch/bootinfo.h>
 #include <smp/smp.h>
 #include <smp/ipi.h>
-#include <arch/atomic.h>
-#include <panic.h>
-#include <print.h>
 
 #ifdef CONFIG_SMP
 
-extern char cpu_by_id_eid_list[256][256];
-
-static void sapic_init(void)
+void ipi_broadcast_arch(int ipi)
 {
-	bootinfo->sapic = (unative_t *)(PA2KA((unative_t)(bootinfo->sapic)) |
-	    FW_OFFSET);
-}
-
-static void ipi_broadcast_arch_all(int ipi)
-{
-	int id, eid;
-	int myid, myeid;
-	
-	myid = ia64_get_cpu_id();
-	myeid = ia64_get_cpu_eid();
-	
-	for (id = 0; id < 256; id++)
-		for (eid = 0; eid < 256; eid++)
-			if ((id != myid) || (eid != myeid))
-				ipi_send_ipi(id, eid, ipi);
-}
-
-void ipi_broadcast_arch(int ipi )
-{
-	int id, eid;
-	int myid, myeid;
-	
-	myid = ia64_get_cpu_id();
-	myeid = ia64_get_cpu_eid();
-
-	for (id = 0; id < 256; id++)
-		for (eid = 0; eid < 256; eid++)
-			if ((id != myid) || (eid != myeid))
-				if (cpu_by_id_eid_list[id][eid])
-					ipi_send_ipi(id, eid, ipi);
 }
 
 void smp_init(void)
 {
-	if (!bootinfo->hello_configured)
-		return; 
-	
-	/*
-	 * If we have not got system prepared by hello, we are not able to start
-	 * AP's. This means we are running on a simulator.
-	 */
-	
-	sapic_init();
-	ipi_broadcast_arch_all(bootinfo->wakeup_intno);	
-	volatile long long brk;
-        for (brk = 0; brk < 100LL * 1024LL * 1024LL; brk++)
-		;	/* wait a while before CPUs starts */
-
-	config.cpu_count = 0;
-	int id, eid;
-	
-	for (id = 0; id < 256; id++)
-		for (eid = 0; eid < 256; eid++)
-		        if (cpu_by_id_eid_list[id][eid] == 1) {
-		    		config.cpu_count++;
-		    		cpu_by_id_eid_list[id][eid] = 2;
-			}
 }
 
 void kmp(void *arg __attribute__((unused)))
 {
-	int id, eid;
-	int myid, myeid;
-	
-	myid = ia64_get_cpu_id();
-	myeid = ia64_get_cpu_eid();
-
-	for (id = 0; id < 256; id++)
-		for (eid = 0; eid < 256; eid++)
-		        if ((id != myid) || (eid != myeid))
-		        	if (cpu_by_id_eid_list[id][eid] != 0) {
-		    			if (cpu_by_id_eid_list[id][eid] == 1) {
-				    		printf("Found Late CPU ID:%d "
-						    "EDI:%d Not added to "
-						    "system!!!\n", id, eid);
-				    		continue;
-					}
-					cpu_by_id_eid_list[id][eid] = 3;
-					/*
-					 * There may be just one AP being
-					 * initialized at the time. After
-					 * it comes completely up, it is
-					 * supposed to wake us up.
-					 */
-					if (waitq_sleep_timeout(
-					    &ap_completion_wq, 1000000,
-					    SYNCH_FLAGS_NONE) ==
-					    ESYNCH_TIMEOUT) {
-						printf("%s: waiting for cpu "
-						    "ID:%d EID:%d timed out\n",
-						    __FUNCTION__, id, eid);
-					    }
-				}
 }
 
 #endif
