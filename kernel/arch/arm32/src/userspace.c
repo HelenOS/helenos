dfd735a17f05114b7c3cfafef153c4894f4737cb
Jiri Svoboda <jiri@wiwaxia>
2010-07-16 13:40:06 +0200

    Separate ldm(2) from instructions accessing banked registers with NOPs. This is forbidden and was corrupting the link register.

--- kernel/arch/arm32/src/userspace.c
+++ kernel/arch/arm32/src/userspace.c
@@ -97,6 +97,7 @@ void userspace(uspace_arg_t *kernel_uarg)
 		"mov sp, %[ustate]\n"
 		"msr spsr_c, %[user_mode]\n"
 		"ldmfd sp, {r0-r12, sp, lr}^\n"
+		"nop\n"		/* Cannot access sp immediately after ldm(2) */
 		"add sp, sp, #(15*4)\n"
 		"ldmfd sp!, {pc}^\n"
 		:: [ustate] "r" (&ustate), [user_mode] "r" (user_mode)
