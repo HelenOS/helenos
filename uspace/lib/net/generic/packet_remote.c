2fa0ad9cca78b8b0aff7a28357c49273d68f8990
Jakub Jermar <jakub@jermar.eu>
2010-09-21 23:02:04 +0200

    Cstyle fixes in libnet.

--- uspace/lib/net/generic/packet_remote.c
+++ uspace/lib/net/generic/packet_remote.c
@@ -63,11 +63,14 @@
  * @return Other error codes as defined for the async_share_in_start() function.
  *
  */
-static int packet_return(int phone, packet_ref packet, packet_id_t packet_id, size_t size){
+static int
+packet_return(int phone, packet_ref packet, packet_id_t packet_id, size_t size)
+{
 	ERROR_DECLARE;
 	
 	ipc_call_t answer;
 	aid_t message = async_send_1(phone, NET_PACKET_GET, packet_id, &answer);
+
 	*packet = (packet_t) as_get_mappable_page(size);
 	if (ERROR_OCCURRED(async_share_in_start_0_0(phone, *packet, size)) ||
 	    ERROR_OCCURRED(pm_add(*packet))) {
@@ -90,16 +93,17 @@ int packet_translate_remote(int phone, packet_ref packet, packet_id_t packet_id)
 		return EINVAL;
 	
 	*packet = pm_find(packet_id);
-	if (!(*packet)) {
+	if (!*packet) {
 		ipcarg_t size;
 		
-		ERROR_PROPAGATE(async_req_1_1(phone, NET_PACKET_GET_SIZE, packet_id, &size));
+		ERROR_PROPAGATE(async_req_1_1(phone, NET_PACKET_GET_SIZE,
+		    packet_id, &size));
 		ERROR_PROPAGATE(packet_return(phone, packet, packet_id, size));
 	}
-	if ((** packet).next) {
+	if ((*packet)->next) {
 		packet_t next;
 		
-		return packet_translate_remote(phone, &next, (** packet).next);
+		return packet_translate_remote(phone, &next, (*packet)->next);
 	}
 	
 	return EOK;
@@ -113,14 +117,15 @@ packet_t packet_get_4_remote(int phone, size_t max_content, size_t addr_len,
 	ipcarg_t packet_id;
 	ipcarg_t size;
 	
-	if (ERROR_OCCURRED(async_req_4_2(phone, NET_PACKET_CREATE_4, max_content,
-	    addr_len, max_prefix, max_suffix, &packet_id, &size)))
+	if (ERROR_OCCURRED(async_req_4_2(phone, NET_PACKET_CREATE_4,
+	    max_content, addr_len, max_prefix, max_suffix, &packet_id, &size)))
 		return NULL;
 	
 	
 	packet_t packet = pm_find(packet_id);
 	if (!packet) {
-		if (ERROR_OCCURRED(packet_return(phone, &packet, packet_id, size)))
+		if (ERROR_OCCURRED(packet_return(phone, &packet, packet_id,
+		    size)))
 			return NULL;
 	}
 	
@@ -140,7 +145,8 @@ packet_t packet_get_1_remote(int phone, size_t content)
 	
 	packet_t packet = pm_find(packet_id);
 	if (!packet) {
-		if (ERROR_OCCURRED(packet_return(phone, &packet, packet_id, size)))
+		if (ERROR_OCCURRED(packet_return(phone, &packet, packet_id,
+		    size)))
 			return NULL;
 	}
 	
