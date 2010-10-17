a8ca1d817fa93ba15a19d092c625d4ed7045f759
Jakub Jermar <jakub@jermar.eu>
2010-10-17 23:34:21 +0200

    Cleanup il local interfaces.

--- uspace/srv/net/il/ip/ip_module.c
+++ uspace/srv/net/il/ip/ip_module.c
@@ -55,25 +55,10 @@
  */
 extern ip_globals_t ip_globals;
 
-/** Processes the IP message.
- *  @param[in] callid The message identifier.
- *  @param[in] call The message parameters.
- *  @param[out] answer The message answer parameters.
- *  @param[out] answer_count The last parameter for the actual answer in the answer parameter.
- *  @returns EOK on success.
- *  @returns Other error codes as defined for the ip_message() function.
- */
 int il_module_message_standalone(ipc_callid_t callid, ipc_call_t * call, ipc_call_t * answer, int * answer_count){
 	return ip_message_standalone(callid, call, answer, answer_count);
 }
 
-/** Starts the IP module.
- *  Initializes the client connection serving function, initializes the module, registers the module service and starts the async manager, processing IPC messages in an infinite loop.
- *  @param[in] client_connection The client connection processing function. The module skeleton propagates its own one.
- *  @returns EOK on successful module termination.
- *  @returns Other error codes as defined for the ip_initialize() function.
- *  @returns Other error codes as defined for the REGISTER_ME() macro function.
- */
 int il_module_start_standalone(async_client_conn_t client_connection){
 	ERROR_DECLARE;
 	
