c77b4d66bf28d81b702cdff70be0e39f1aaac448
Jakub Jermar <jakub@jermar.eu>
2010-10-23 18:19:51 +0200

    Remove unused LSAP definitions.

--- uspace/lib/net/include/ethernet_lsap.h
+++ uspace/lib/net/include/ethernet_lsap.h
@@ -27,106 +27,33 @@
  */
 
 /** @addtogroup eth
- *  @{
+ * @{
  */
 
 /** @file
- *  Link service access point identifiers.
+ * Link service access point identifiers.
  */
 
-#ifndef __NET_ETHERNET_LSAP_H__
-#define __NET_ETHERNET_LSAP_H__
+#ifndef LIBNET_ETHERNET_LSAP_H_
+#define LIBNET_ETHERNET_LSAP_H_
 
 #include <sys/types.h>
 
-/** Ethernet LSAP type definition.
- */
-typedef uint8_t	eth_lsap_t;
+/** Ethernet LSAP type definition. */
+typedef uint8_t eth_lsap_t;
 
-/** @name Ethernet LSAP values definitions
- */
+/** @name Ethernet LSAP values definitions */
 /*@{*/
 
-/** Null LSAP LSAP identifier.
- */
+/** Null LSAP LSAP identifier. */
 #define ETH_LSAP_NULL	0x00
-/** Individual LLC Sublayer Management Function LSAP identifier.
- */
-#define ETH_LSAP_ISLMF	0x02
-/** Group LLC Sublayer Management Function LSAP identifier.
- */
-#define ETH_LSAP_GSLMI	0x03
-/** IBM SNA Path Control (individual) LSAP identifier.
- */
-#define ETH_LSAP_ISNA	0x04
-/** IBM SNA Path Control (group) LSAP identifier.
- */
-#define ETH_LSAP_GSNA	0x05
-/** ARPANET Internet Protocol (IP) LSAP identifier.
- */
+/** ARPANET Internet Protocol (IP) LSAP identifier. */
 #define ETH_LSAP_IP	0x06
-/** SNA LSAP identifier.
- */
-#define ETH_LSAP_SNA	0x08
-/** SNA LSAP identifier.
- */
-#define ETH_LSAP_SNA2	0x0C
-/** PROWAY (IEC955) Network Management &Initialization LSAP identifier.
- */
-#define ETH_LSAP_PROWAY_NMI	0x0E
-/** Texas Instruments LSAP identifier.
- */
-#define ETH_LSAP_TI	0x18
-/** IEEE 802.1 Bridge Spanning Tree Protocol LSAP identifier.
- */
-#define ETH_LSAP_BRIDGE	0x42
-/** EIA RS-511 Manufacturing Message Service LSAP identifier.
- */
-#define ETH_LSAP_EIS	0x4E
-/** ISO 8208 (X.25 over IEEE 802.2 Type 2 LLC) LSAP identifier.
- */
-#define ETH_LSAP_ISO8208	0x7E
-/** Xerox Network Systems (XNS) LSAP identifier.
- */
-#define ETH_LSAP_XNS	0x80
-/** Nestar LSAP identifier.
- */
-#define ETH_LSAP_NESTAR	0x86
-/** PROWAY (IEC 955) Active Station List Maintenance LSAP identifier.
- */
-#define ETH_LSAP_PROWAY_ASLM	0x8E
-/** ARPANET Address Resolution Protocol (ARP) LSAP identifier.
- */
+/** ARPANET Address Resolution Protocol (ARP) LSAP identifier. */
 #define ETH_LSAP_ARP	0x98
-/** Banyan VINES LSAP identifier.
- */
-#define ETH_LSAP_VINES	0xBC
-/** SubNetwork Access Protocol (SNAP) LSAP identifier.
- */
+/** SubNetwork Access Protocol (SNAP) LSAP identifier. */
 #define ETH_LSAP_SNAP	0xAA
-/** Novell NetWare LSAP identifier.
- */
-#define ETH_LSAP_NETWARE	0xE0
-/** IBM NetBIOS LSAP identifier.
- */
-#define ETH_LSAP_NETBIOS	0xF0
-/** IBM LAN Management (individual) LSAP identifier.
- */
-#define ETH_LSAP_ILAN	0xF4
-/** IBM LAN Management (group) LSAP identifier.
- */
-#define ETH_LSAP_GLAN	0xF5
-/** IBM Remote Program Load (RPL) LSAP identifier.
- */
-#define ETH_LSAP_RPL	0xF8
-/** Ungermann-Bass LSAP identifier.
- */
-#define ETH_LSAP_UB	0xFA
-/** ISO Network Layer Protocol LSAP identifier.
- */
-#define ETH_LSAP_ISONLP	0xFE
-/** Global LSAP LSAP identifier.
- */
+/** Global LSAP LSAP identifier. */
 #define ETH_LSAP_GLSAP	0xFF
 
 /*@}*/
