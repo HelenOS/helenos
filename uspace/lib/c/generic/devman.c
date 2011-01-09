622dea83a1bf05d07d10fd5756c5f5757a2f4b12
Jakub Jermar <jakub@jermar.eu>
2011-01-09 22:14:19 +0100

    Protect devman_get_phone() by a mutex.

--- uspace/lib/c/generic/devman.c
+++ uspace/lib/c/generic/devman.c
@@ -41,6 +41,7 @@
 #include <ipc/devman.h>
 #include <devman.h>
 #include <async.h>
+#include <fibril_synch.h>
 #include <errno.h>
 #include <malloc.h>
 #include <bool.h>
@@ -49,12 +50,17 @@
 static int devman_phone_driver = -1;
 static int devman_phone_client = -1;
 
+static FIBRIL_MUTEX_INITIALIZE(devman_phone_mutex);
+
 int devman_get_phone(devman_interface_t iface, unsigned int flags)
 {
 	switch (iface) {
 	case DEVMAN_DRIVER:
-		if (devman_phone_driver >= 0)
+		fibril_mutex_lock(&devman_phone_mutex);
+		if (devman_phone_driver >= 0) {
+			fibril_mutex_unlock(&devman_phone_mutex);
 			return devman_phone_driver;
+		}
 		
 		if (flags & IPC_FLAG_BLOCKING)
 			devman_phone_driver = async_connect_me_to_blocking(
@@ -63,10 +69,14 @@ int devman_get_phone(devman_interface_t iface, unsigned int flags)
 			devman_phone_driver = async_connect_me_to(PHONE_NS,
 			    SERVICE_DEVMAN, DEVMAN_DRIVER, 0);
 		
+		fibril_mutex_unlock(&devman_phone_mutex);
 		return devman_phone_driver;
 	case DEVMAN_CLIENT:
-		if (devman_phone_client >= 0)
+		fibril_mutex_lock(&devman_phone_mutex);
+		if (devman_phone_client >= 0) {
+			fibril_mutex_unlock(&devman_phone_mutex);
 			return devman_phone_client;
+		}
 		
 		if (flags & IPC_FLAG_BLOCKING)
 			devman_phone_client = async_connect_me_to_blocking(
@@ -74,7 +84,9 @@ int devman_get_phone(devman_interface_t iface, unsigned int flags)
 		else
 			devman_phone_client = async_connect_me_to(PHONE_NS,
 			    SERVICE_DEVMAN, DEVMAN_CLIENT, 0);
+			fibril_mutex_unlock(&devman_phone_mutex);
 		
+		fibril_mutex_unlock(&devman_phone_mutex);
 		return devman_phone_client;
 	default:
 		return -1;
