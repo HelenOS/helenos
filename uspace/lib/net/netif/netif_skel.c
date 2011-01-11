4fc2b3bfdba83c97d0b6715c3e6412d29f2af0d6
Martin Decky <martin@ares.decky.cz>
2011-01-11 02:31:36 +0100

    do not unlock the RW-lock until the changes in NIL modules have finished

--- uspace/lib/net/netif/netif_skel.c
+++ uspace/lib/net/netif/netif_skel.c
@@ -129,8 +129,8 @@ static int netif_start_req_local(int netif_phone, device_id_t device_id)
 	int result = netif_start_message(device);
 	if (result > NETIF_NULL) {
 		int phone = device->nil_phone;
-		fibril_rwlock_write_unlock(&netif_globals.lock);
 		nil_device_state_msg(phone, device_id, result);
+		fibril_rwlock_write_unlock(&netif_globals.lock);
 		return EOK;
 	}
 	
@@ -165,8 +165,8 @@ static int netif_stop_req_local(int netif_phone, device_id_t device_id)
 	int result = netif_stop_message(device);
 	if (result > NETIF_NULL) {
 		int phone = device->nil_phone;
-		fibril_rwlock_write_unlock(&netif_globals.lock);
 		nil_device_state_msg(phone, device_id, result);
+		fibril_rwlock_write_unlock(&netif_globals.lock);
 		return EOK;
 	}
 	
