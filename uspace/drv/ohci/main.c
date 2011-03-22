11dd29b9dfc8a1f985d6d602a25f92e15519c89c
Vojtech Horky <vojtechhorky@users.sourceforge.net>
2011-03-22 16:26:35 +0100

    Using interrupts in OHCI and UHCI optional
    Added option to HelenOS config to allow enforced usage of polling.
    
    Shall bypass problems discussed on HelenOS mailing list recently:
    http://lists.modry.cz/cgi-bin/private/helenos-devel/2011-March/007038.html

--- uspace/drv/ohci/main.c
+++ uspace/drv/ohci/main.c
@@ -148,16 +148,23 @@ if (ret != EOK) { \
 		return ENOMEM;
 	}
 
+
 	bool interrupts = false;
+#ifdef CONFIG_USBHC_NO_INTERRUPTS
+	usb_log_warning("Interrupts disabled in OS config, " \
+	    "falling back to polling.\n");
+#else
 	ret = pci_enable_interrupts(device);
 	if (ret != EOK) {
-		usb_log_warning(
-		    "Failed(%d) to enable interrupts, fall back to polling.\n",
-		    ret);
+		usb_log_warning("Failed to enable interrupts: %s.\n",
+		    str_error(rc));
+		usb_log_info("HW interrupts not available, " \
+		    "falling back to polling.\n");
 	} else {
 		usb_log_debug("Hw interrupts enabled.\n");
 		interrupts = true;
 	}
+#endif
 
 	ret = hc_init(hcd, hc_fun, device, mem_reg_base, mem_reg_size, interrupts);
 	if (ret != EOK) {
