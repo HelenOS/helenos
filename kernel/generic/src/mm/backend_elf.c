563b5385ab991fa17ac41d61832cf43e1c5c58ea
Martin Decky <martin@decky.cz>
2011-03-30 17:39:07 +0200

    never use top-level conjunction in assertions, always write two assertions

--- kernel/generic/src/mm/backend_elf.c
+++ kernel/generic/src/mm/backend_elf.c
@@ -91,8 +91,8 @@ int elf_page_fault(as_area_t *area, uintptr_t addr, pf_access_t access)
 	if (!as_area_check_access(area, access))
 		return AS_PF_FAULT;
 
-	ASSERT((addr >= ALIGN_DOWN(entry->p_vaddr, PAGE_SIZE)) &&
-	    (addr < entry->p_vaddr + entry->p_memsz));
+	ASSERT(addr >= ALIGN_DOWN(entry->p_vaddr, PAGE_SIZE));
+	ASSERT(addr < entry->p_vaddr + entry->p_memsz);
 	i = (addr - ALIGN_DOWN(entry->p_vaddr, PAGE_SIZE)) >> PAGE_WIDTH;
 	base = (uintptr_t)
 	    (((void *) elf) + ALIGN_DOWN(entry->p_offset, PAGE_SIZE));
