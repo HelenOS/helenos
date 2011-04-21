010be4766a548f70858da667fe7240c5f7f5e4e0
Jakub Jermar <jakub@jermar.eu>
2011-04-21 21:27:02 +0200

    In case of no reserve in reserve_try_alloc(), attempt to reclaim reservable memory from the slab allocator.
    Add doxygen comments.

--- kernel/generic/src/mm/reserve.c
+++ kernel/generic/src/mm/reserve.c
@@ -37,6 +37,7 @@
 
 #include <mm/reserve.h>
 #include <mm/frame.h>
+#include <mm/slab.h>
 #include <synch/spinlock.h>
 #include <typedefs.h>
 #include <arch/types.h>
@@ -44,6 +45,14 @@
 IRQ_SPINLOCK_STATIC_INITIALIZE_NAME(reserve_lock, "reserve_lock");
 static ssize_t reserve = 0;
 
+/** Try to reserve memory.
+ *
+ * This function may not be called from contexts that do not allow memory
+ * reclaiming, such as some invocations of frame_alloc_generic().
+ *
+ * @param size		Number of frames to reserve.
+ * @return		True on success or false otherwise.
+ */
 bool reserve_try_alloc(size_t size)
 {
 	bool reserved = false;
@@ -52,12 +61,43 @@ bool reserve_try_alloc(size_t size)
 	if (reserve >= 0 && (size_t) reserve >= size) {
 		reserve -= size;
 		reserved = true;
+	} else {
+		/*
+		 * Some reservable frames may be cached by the slab allocator.
+		 * Try to reclaim some reservable memory. Try to be gentle for
+		 * the first time. If it does not help, try to reclaim
+		 * everything.
+		 */
+		irq_spinlock_unlock(&reserve_lock, true);
+		slab_reclaim(0);
+		irq_spinlock_lock(&reserve_lock, true);
+		if (reserve >= 0 && (size_t) reserve >= size) {
+			reserve -= size;
+			reserved = true;
+		} else {
+			irq_spinlock_unlock(&reserve_lock, true);
+			slab_reclaim(SLAB_RECLAIM_ALL);
+			irq_spinlock_lock(&reserve_lock, true);
+			if (reserve >= 0 && (size_t) reserve >= size) {
+				reserve -= size;
+				reserved = true;
+			}
+		}
 	}
 	irq_spinlock_unlock(&reserve_lock, true);
 
 	return reserved;
 }
 
+/** Reserve memory.
+ *
+ * This function simply marks the respective amount of memory frames reserved.
+ * It does not implement any sort of blocking for the case there is not enough
+ * reservable memory. It will simply take the reserve into negative numbers and
+ * leave the blocking up to the allocation phase.
+ *
+ * @param size		Number of frames to reserve.
+ */
 void reserve_force_alloc(size_t size)
 {
 	irq_spinlock_lock(&reserve_lock, true);
@@ -65,6 +105,10 @@ void reserve_force_alloc(size_t size)
 	irq_spinlock_unlock(&reserve_lock, true);
 }
 
+/** Unreserve memory.
+ *
+ * @param size		Number of frames to unreserve.
+ */
 void reserve_free(size_t size)
 {
 	irq_spinlock_lock(&reserve_lock, true);
