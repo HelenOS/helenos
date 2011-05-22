b4ffe5bc5eb3ffce96cc4d6093843f6d99cd1144
Jakub Jermar <jakub@jermar.eu>
2011-05-22 21:33:15 +0200

    Use the P2SZ() macro in address space area backends.

--- kernel/generic/src/mm/backend_anon.c
+++ kernel/generic/src/mm/backend_anon.c
@@ -121,11 +121,11 @@ void anon_share(as_area_t *area)
 			
 				page_table_lock(area->as, false);
 				pte = page_mapping_find(area->as,
-				    base + j * PAGE_SIZE, false);
+				    base + P2SZ(j), false);
 				ASSERT(pte && PTE_VALID(pte) &&
 				    PTE_PRESENT(pte));
 				btree_insert(&area->sh_info->pagemap,
-				    (base + j * PAGE_SIZE) - area->base,
+				    (base + P2SZ(j)) - area->base,
 				    (void *) PTE_GET_FRAME(pte), NULL);
 				page_table_unlock(area->as, false);
 
