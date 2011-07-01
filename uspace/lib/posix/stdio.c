1978a5fa6a143ff2f26ae41fe890b8cb99bc4066
Petr Koupy <petr.koupy@gmail.com>
2011-07-02 01:39:27 +0200

    Implemented ungetc() and commented some functions from stdio.c.

--- uspace/lib/posix/stdio.c
+++ uspace/lib/posix/stdio.c
@@ -37,21 +37,23 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <bool.h>
 
 #include "internal/common.h"
 #include "stdio.h"
 #include "libc/io/printf_core.h"
 #include "string.h"
 #include "libc/str.h"
+#include "sys/types.h"
 
-/* not the best of solutions, but freopen will eventually
+/* not the best of solutions, but freopen and ungetc will eventually
  * need to be implemented in libc anyway
  */
 #include "../c/generic/private/stdio.h"
 
 /** Clears the stream's error and end-of-file indicators.
  *
- * @param stream
+ * @param stream Stream whose indicators shall be cleared.
  */
 void posix_clearerr(FILE *stream)
 {
@@ -60,9 +62,10 @@ void posix_clearerr(FILE *stream)
 }
 
 /**
+ * Generate a pathname for the controlling terminal.
  *
- * @param s
- * @return
+ * @param s Allocated buffer to which the pathname shall be put.
+ * @return Either s or static location filled with the requested pathname.
  */
 char *posix_ctermid(char *s)
 {
@@ -80,17 +83,53 @@ char *posix_ctermid(char *s)
 }
 
 /**
+ * Push byte back into input stream.
  * 
- * @param c
- * @param stream
- * @return 
+ * @param c Byte to be pushed back.
+ * @param stream Stream to where the byte shall be pushed.
+ * @return Provided byte on succes or EOF if not possible.
  */
 int posix_ungetc(int c, FILE *stream)
 {
-	// TODO
-	not_implemented();
+	uint8_t b = (uint8_t) c;
+
+	bool can_unget =
+		/* Provided character is legal. */
+	    c != EOF &&
+		/* Stream is consistent. */
+	    !stream->error &&
+		/* Stream is buffered. */
+	    stream->btype != _IONBF &&
+		/* Last operation on the stream was a read operation. */
+	    stream->buf_state == _bs_read &&
+		/* Stream buffer is already allocated (i.e. there was already carried
+		 * out either write or read operation on the stream). This is probably
+		 * redundant check but let's be safe. */
+	    stream->buf != NULL &&
+		/* There is still space in the stream to retreat. POSIX demands the
+		 * possibility to unget at least 1 character. It should be always
+		 * possible, assuming the last operation on the stream read at least 1
+		 * character, because the buffer is refilled in the lazily manner. */
+	    stream->buf_tail > stream->buf;
+
+	if (can_unget) {
+		--stream->buf_tail;
+		stream->buf_tail[0] = b;
+		stream->eof = false;
+		return (int) b;
+	} else {
+		return EOF;
+	}
 }
 
+/**
+ *
+ * @param lineptr
+ * @param n
+ * @param delimiter
+ * @param stream
+ * @return
+ */
 ssize_t posix_getdelim(char **restrict lineptr, size_t *restrict n,
     int delimiter, FILE *restrict stream)
 {
@@ -98,6 +137,13 @@ ssize_t posix_getdelim(char **restrict lineptr, size_t *restrict n,
 	not_implemented();
 }
 
+/**
+ * 
+ * @param lineptr
+ * @param n
+ * @param stream
+ * @return
+ */
 ssize_t posix_getline(char **restrict lineptr, size_t *restrict n,
     FILE *restrict stream)
 {
@@ -106,11 +152,15 @@ ssize_t posix_getline(char **restrict lineptr, size_t *restrict n,
 }
 
 /**
+ * Reopen a file stream.
  * 
- * @param filename
- * @param mode
- * @param stream
- * @return
+ * @param filename Pathname of a file to be reopened or NULL for changing
+ *     the mode of the stream.
+ * @param mode Mode to be used for reopening the file or changing current
+ *     mode of the stream.
+ * @param stream Current stream associated with the opened file.
+ * @return On success, either a stream of the reopened file or the provided
+ *     stream with a changed mode. NULL otherwise.
  */
 FILE *posix_freopen(
     const char *restrict filename,
@@ -158,6 +208,13 @@ FILE *posix_freopen(
 	return stream;
 }
 
+/**
+ *
+ * @param buf
+ * @param size
+ * @param mode
+ * @return
+ */
 FILE *posix_fmemopen(void *restrict buf, size_t size,
     const char *restrict mode)
 {
@@ -165,6 +222,12 @@ FILE *posix_fmemopen(void *restrict buf, size_t size,
 	not_implemented();
 }
 
+/**
+ *
+ * @param bufp
+ * @param sizep
+ * @return
+ */
 FILE *posix_open_memstream(char **bufp, size_t *sizep)
 {
 	// TODO
@@ -172,8 +235,9 @@ FILE *posix_open_memstream(char **bufp, size_t *sizep)
 }
 
 /**
+ * Write error messages to standard error.
  *
- * @param s
+ * @param s Error message.
  */
 void posix_perror(const char *s)
 {
@@ -216,11 +280,12 @@ int posix_fgetpos(FILE *restrict stream, posix_fpos_t *restrict pos)
 }
 
 /**
+ * Reposition a file-position indicator in a stream.
  * 
- * @param stream
- * @param offset
- * @param whence
- * @return
+ * @param stream Stream to seek in.
+ * @param offset Direction and amount of bytes to seek.
+ * @param whence From where to seek.
+ * @return Zero on success, -1 otherwise.
  */
 int posix_fseek(FILE *stream, long offset, int whence)
 {
@@ -228,11 +293,12 @@ int posix_fseek(FILE *stream, long offset, int whence)
 }
 
 /**
+ * Reposition a file-position indicator in a stream.
  * 
- * @param stream
- * @param offset
- * @param whence
- * @return
+ * @param stream Stream to seek in.
+ * @param offset Direction and amount of bytes to seek.
+ * @param whence From where to seek.
+ * @return Zero on success, -1 otherwise.
  */
 int posix_fseeko(FILE *stream, posix_off_t offset, int whence)
 {
@@ -240,9 +306,10 @@ int posix_fseeko(FILE *stream, posix_off_t offset, int whence)
 }
 
 /**
+ * Discover current file offset in a stream.
  * 
- * @param stream
- * @return
+ * @param stream Stream for which the offset shall be retrieved.
+ * @return Current offset or -1 if not possible.
  */
 long posix_ftell(FILE *stream)
 {
@@ -250,15 +317,23 @@ long posix_ftell(FILE *stream)
 }
 
 /**
+ * Discover current file offset in a stream.
  * 
- * @param stream
- * @return
+ * @param stream Stream for which the offset shall be retrieved.
+ * @return Current offset or -1 if not possible.
  */
 posix_off_t posix_ftello(FILE *stream)
 {
 	return (posix_off_t) ftell(stream);
 }
 
+/**
+ * Print formatted output to the opened file.
+ *
+ * @param fildes File descriptor of the opened file.
+ * @param format Format description.
+ * @return Either the number of printed characters or negative value on error.
+ */
 int posix_dprintf(int fildes, const char *restrict format, ...)
 {
 	va_list list;
@@ -268,12 +343,28 @@ int posix_dprintf(int fildes, const char *restrict format, ...)
 	return result;
 }
 
+/**
+ * Write ordinary string to the opened file.
+ *
+ * @param str String to be written.
+ * @param size Size of the string (in bytes)..
+ * @param fd File descriptor of the opened file.
+ * @return The number of written characters.
+ */
 static int _dprintf_str_write(const char *str, size_t size, void *fd)
 {
 	ssize_t wr = write(*(int *) fd, str, size);
 	return str_nlength(str, wr);
 }
 
+/**
+ * Write wide string to the opened file.
+ * 
+ * @param str String to be written.
+ * @param size Size of the string (in bytes).
+ * @param fd File descriptor of the opened file.
+ * @return The number of written characters.
+ */
 static int _dprintf_wstr_write(const wchar_t *str, size_t size, void *fd)
 {
 	size_t offset = 0;
@@ -298,6 +389,14 @@ static int _dprintf_wstr_write(const wchar_t *str, size_t size, void *fd)
 	return chars;
 }
 
+/**
+ * Print formatted output to the opened file.
+ * 
+ * @param fildes File descriptor of the opened file.
+ * @param format Format description.
+ * @param ap Print arguments.
+ * @return Either the number of printed characters or negative value on error.
+ */
 int posix_vdprintf(int fildes, const char *restrict format, va_list ap)
 {
 	printf_spec_t spec = {
@@ -310,11 +409,12 @@ int posix_vdprintf(int fildes, const char *restrict format, va_list ap)
 }
 
 /**
+ * Print formatted output to the string.
  * 
- * @param s
- * @param format
- * @param ...
- * @return
+ * @param s Output string.
+ * @param format Format description.
+ * @return Either the number of printed characters (excluding null byte) or
+ *     negative value on error.
  */
 int posix_sprintf(char *s, const char *format, ...)
 {
@@ -326,11 +426,13 @@ int posix_sprintf(char *s, const char *format, ...)
 }
 
 /**
+ * Print formatted output to the string.
  * 
- * @param s
- * @param format
- * @param ap
- * @return
+ * @param s Output string.
+ * @param format Format description.
+ * @param ap Print arguments.
+ * @return Either the number of printed characters (excluding null byte) or
+ *     negative value on error.
  */
 int posix_vsprintf(char *s, const char *format, va_list ap)
 {
@@ -338,11 +440,11 @@ int posix_vsprintf(char *s, const char *format, va_list ap)
 }
 
 /**
+ * Convert formatted input from the stream.
  * 
- * @param stream
- * @param format
- * @param ...
- * @return
+ * @param stream Input stream.
+ * @param format Format description.
+ * @return The number of converted output items or EOF on failure.
  */
 int posix_fscanf(FILE *restrict stream, const char *restrict format, ...)
 {
@@ -353,6 +455,14 @@ int posix_fscanf(FILE *restrict stream, const char *restrict format, ...)
 	return result;
 }
 
+/**
+ * Convert formatted input from the stream.
+ * 
+ * @param stream Input stream.
+ * @param format Format description.
+ * @param arg Output items.
+ * @return The number of converted output items or EOF on failure.
+ */
 int posix_vfscanf(FILE *restrict stream, const char *restrict format, va_list arg)
 {
 	// TODO
@@ -360,10 +470,10 @@ int posix_vfscanf(FILE *restrict stream, const char *restrict format, va_list ar
 }
 
 /**
+ * Convert formatted input from the standard input.
  * 
- * @param format
- * @param ...
- * @return
+ * @param format Format description.
+ * @return The number of converted output items or EOF on failure.
  */
 int posix_scanf(const char *restrict format, ...)
 {
@@ -375,10 +485,11 @@ int posix_scanf(const char *restrict format, ...)
 }
 
 /**
+ * Convert formatted input from the standard input.
  * 
- * @param format
- * @param arg
- * @return
+ * @param format Format description.
+ * @param arg Output items.
+ * @return The number of converted output items or EOF on failure.
  */
 int posix_vscanf(const char *restrict format, va_list arg)
 {
@@ -386,11 +497,11 @@ int posix_vscanf(const char *restrict format, va_list arg)
 }
 
 /**
+ * Convert formatted input from the string.
  * 
- * @param s
- * @param format
- * @param ...
- * @return
+ * @param s Input string.
+ * @param format Format description.
+ * @return The number of converted output items or EOF on failure.
  */
 int posix_sscanf(const char *s, const char *format, ...)
 {
@@ -402,11 +513,12 @@ int posix_sscanf(const char *s, const char *format, ...)
 }
 
 /**
+ * Convert formatted input from the string.
  * 
- * @param s
- * @param format
- * @param arg
- * @return
+ * @param s Input string.
+ * @param format Format description.
+ * @param arg Output items.
+ * @return The number of converted output items or EOF on failure.
  */
 int posix_vsscanf(
     const char *restrict s, const char *restrict format, va_list arg)
@@ -415,46 +527,87 @@ int posix_vsscanf(
 	not_implemented();
 }
 
+/**
+ * Acquire file stream for the thread.
+ *
+ * @param file File stream to lock.
+ */
 void posix_flockfile(FILE *file)
 {
 	/* dummy */
 }
 
+/**
+ * Acquire file stream for the thread (non-blocking).
+ *
+ * @param file File stream to lock.
+ * @return Zero for success and non-zero if the lock cannot be acquired.
+ */
 int posix_ftrylockfile(FILE *file)
 {
 	/* dummy */
 	return 0;
 }
 
+/**
+ * Relinquish the ownership of the locked file stream.
+ *
+ * @param file File stream to unlock.
+ */
 void posix_funlockfile(FILE *file)
 {
 	/* dummy */
 }
 
+/**
+ * Get a byte from a stream (thread-unsafe).
+ *
+ * @param stream Input file stream.
+ * @return Either read byte or EOF.
+ */
 int posix_getc_unlocked(FILE *stream)
 {
 	return getc(stream);
 }
 
+/**
+ * Get a byte from the standard input stream (thread-unsafe).
+ *
+ * @return Either read byte or EOF.
+ */
 int posix_getchar_unlocked(void)
 {
 	return getchar();
 }
 
+/**
+ * Put a byte on a stream (thread-unsafe).
+ *
+ * @param c Byte to output.
+ * @param stream Output file stream.
+ * @return Either written byte or EOF.
+ */
 int posix_putc_unlocked(int c, FILE *stream)
 {
 	return putc(c, stream);
 }
 
+/**
+ * Put a byte on the standard output stream (thread-unsafe).
+ * 
+ * @param c Byte to output.
+ * @return Either written byte or EOF.
+ */
 int posix_putchar_unlocked(int c)
 {
 	return putchar(c);
 }
 
 /**
+ * Remove a file.
  *
- * @param path
- * @return
+ * @param path Pathname of the file that shall be removed.
+ * @return Zero on success, -1 otherwise.
  */
 int posix_remove(const char *path)
 {
