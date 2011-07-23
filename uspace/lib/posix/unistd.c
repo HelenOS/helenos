c62f2d0b67269b065b7ba128b6de649871b0be11
Petr Koupy <petr.koupy@gmail.com>
2011-07-23 22:25:46 +0200

    Corrected merge conflict in unistd.c.

--- uspace/lib/posix/unistd.c
+++ uspace/lib/posix/unistd.c
@@ -88,13 +88,8 @@ int posix_getlogin_r(char *name, size_t namesize)
 int posix_isatty(int fd)
 {
 	/* Always returns false, because there is no easy way to find
-<<<<<<< TREE
      * out under HelenOS. */
 	return 0;
-=======
-	 * out under HelenOS. */
-	return false;
->>>>>>> MERGE-SOURCE
 }
 
 /**
