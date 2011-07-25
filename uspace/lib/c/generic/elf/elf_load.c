c56678277933bfe914b5c5b5102db476b258b8ba
Jakub Jermar <jakub@jermar.eu>
2011-07-25 22:24:56 +0200

    Use read_all() in elf_load.c.

--- uspace/lib/c/generic/elf/elf_load.c
+++ uspace/lib/c/generic/elf/elf_load.c
@@ -73,19 +73,6 @@ static int segment_header(elf_ld_t *elf, elf_segment_header_t *entry);
 static int section_header(elf_ld_t *elf, elf_section_header_t *entry);
 static int load_segment(elf_ld_t *elf, elf_segment_header_t *entry);
 
-/** Read until the buffer is read in its entirety. */
-static int my_read(int fd, void *buf, size_t len)
-{
-	int cnt = 0;
-	do {
-		buf += cnt;
-		len -= cnt;
-		cnt = read(fd, buf, len);
-	} while ((cnt > 0) && ((len - cnt) > 0));
-
-	return cnt;
-}
-
 /** Load ELF binary from a file.
  *
  * Load an ELF binary from the specified file. If the file is
@@ -159,7 +146,7 @@ static unsigned int elf_load(elf_ld_t *elf, size_t so_bias)
 	elf_header_t *header = &header_buf;
 	int i, rc;
 
-	rc = my_read(elf->fd, header, sizeof(elf_header_t));
+	rc = read_all(elf->fd, header, sizeof(elf_header_t));
 	if (rc < 0) {
 		DPRINTF("Read error.\n"); 
 		return EE_INVALID;
@@ -221,7 +208,7 @@ static unsigned int elf_load(elf_ld_t *elf, size_t so_bias)
 		lseek(elf->fd, header->e_phoff
 		        + i * sizeof(elf_segment_header_t), SEEK_SET);
 
-		rc = my_read(elf->fd, &segment_hdr,
+		rc = read_all(elf->fd, &segment_hdr,
 		    sizeof(elf_segment_header_t));
 		if (rc < 0) {
 			DPRINTF("Read error.\n");
@@ -243,7 +230,7 @@ static unsigned int elf_load(elf_ld_t *elf, size_t so_bias)
 		lseek(elf->fd, header->e_shoff
 		    + i * sizeof(elf_section_header_t), SEEK_SET);
 
-		rc = my_read(elf->fd, &section_hdr,
+		rc = read_all(elf->fd, &section_hdr,
 		    sizeof(elf_section_header_t));
 		if (rc < 0) {
 			DPRINTF("Read error.\n");
@@ -411,7 +398,7 @@ int load_segment(elf_ld_t *elf, elf_segment_header_t *entry)
 		now = 16384;
 		if (now > left) now = left;
 
-		rc = my_read(elf->fd, dp, now);
+		rc = read_all(elf->fd, dp, now);
 
 		if (rc < 0) { 
 			DPRINTF("Read error.\n");
