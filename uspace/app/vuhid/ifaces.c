967447658a7c74fa9716dc3e4d51ef50eb46c052
Vojtech Horky <vojtechhorky@users.sourceforge.net>
2011-09-20 22:55:06 +0200

    Add Logitech Wireless as virtual USB HID (#349)
    Issue (copy from Trac):
    When testing the USB framework (mainline,1035) on my
    desktop machine, I hit the following malloc assertion
    in the usbhid task when having moved the mouse for
    the first time after it attached:
    
    assert_abort+93
    malloc_area+1021
    malloc_internal+198
    malloc+32
    usb_hid_report_path_append_item+23
    usb_hid_report_get_sibling+124
    usb_mouse_polling_callback+1000
    usb_hid_polling_callback+158
    --end of issue description
    
    The device causing the trouble is
    046d:c52b Logitech, Inc. Unifying Receiver
    its interface 1 (mouse).
    
    The virtual USB HID simulates plug-in of the interface.
    That currently causes an assertion failure in usbhid driver
    in a reproducible way (i.e. without having that particular
    piece of hardware).
    Following revisions shall fix the bug ;-).

--- uspace/app/vuhid/ifaces.c
+++ uspace/app/vuhid/ifaces.c
@@ -37,9 +37,11 @@
 #include "ifaces.h"
 
 extern vuhid_interface_t vuhid_interface_bootkbd;
+extern vuhid_interface_t vuhid_interface_logitech_wireless_1;
 
 vuhid_interface_t *available_hid_interfaces[] = {
 	&vuhid_interface_bootkbd,
+	&vuhid_interface_logitech_wireless_1,
 	NULL
 };
 
