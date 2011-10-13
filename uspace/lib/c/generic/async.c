cff3fb6c3220d53e3f075f4b038425266c3ad1ad
Jan Vesely <jano.vesely@gmail.com>
2011-10-13 13:16:00 +0200

    sync_hangup: Try the one phone we care about first.

--- uspace/lib/c/generic/async.c
+++ uspace/lib/c/generic/async.c
@@ -1845,6 +1845,8 @@ int async_hangup(async_sess_t *sess)
 		return EBUSY;
 	
 	fibril_mutex_lock(&async_sess_mutex);
+
+	int rc = async_hangup_internal(sess->phone);
 	
 	while (!list_empty(&sess->exch_list)) {
 		exch = (async_exch_t *)
@@ -1857,7 +1859,6 @@ int async_hangup(async_sess_t *sess)
 		free(exch);
 	}
 
-	int rc = async_hangup_internal(sess->phone);
 	free(sess);
 	
 	fibril_mutex_unlock(&async_sess_mutex);
