a044f71edaa5af02881a5f002ebce9c2107d9d03
Jakub Jermar <jakub@jermar.eu>
2011-10-15 20:55:07 +0200

    Device driver data cannot be freed by the driver itself.

--- uspace/drv/bus/usb/usbmast/main.c
+++ uspace/drv/bus/usb/usbmast/main.c
@@ -163,7 +163,10 @@ error:
 		ddf_fun_destroy(mdev->luns[i]);
 	}
 	free(mdev->luns);
-	free(mdev);
+	/*
+	 * Do not free mdev here as it was passed to DDF as device driver data.
+	 * It will be deallocated by DDF when the device is destroyed.
+	 */
 	return rc;
 }
 
