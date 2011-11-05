d176b1d58f235c1bb1c61b96a452dd26a39de9b4
Jan Vesely <jano.vesely@gmail.com>
2011-11-05 18:21:40 +0100

    usbmast: Use pipe_t structure to get max_packet_size information.

--- uspace/drv/bus/usb/usbmast/main.c
+++ uspace/drv/bus/usb/usbmast/main.c
@@ -141,10 +141,10 @@ static int usbmast_device_add(usb_device_t *dev)
 	usb_log_info("Initializing mass storage `%s'.\n", dev->ddf_dev->name);
 	usb_log_debug("Bulk in endpoint: %d [%zuB].\n",
 	    dev->pipes[BULK_IN_EP].pipe.endpoint_no,
-	    (size_t) dev->pipes[BULK_IN_EP].descriptor->max_packet_size);
+	    dev->pipes[BULK_IN_EP].pipe.max_packet_size);
 	usb_log_debug("Bulk out endpoint: %d [%zuB].\n",
 	    dev->pipes[BULK_OUT_EP].pipe.endpoint_no,
-	    (size_t) dev->pipes[BULK_OUT_EP].descriptor->max_packet_size);
+	    dev->pipes[BULK_OUT_EP].pipe.max_packet_size);
 
 	usb_log_debug("Get LUN count...\n");
 	mdev->lun_count = usb_masstor_get_lun_count(mdev);
