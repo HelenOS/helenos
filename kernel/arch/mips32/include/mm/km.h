bc428e23afb77940e4c15db93c361ee6efc12ac7
Jakub Jermar <jakub@jermar.eu>
2011-11-09 00:24:17 +0100

    Partition mips32 kernel memory.

--- kernel/arch/mips32/include/mm/km.h
+++ kernel/arch/mips32/include/mm/km.h
@@ -35,6 +35,15 @@
 #ifndef KERN_mips32_KM_H_
 #define KERN_mips32_KM_H_
 
+#define KM_MIPS32_KSEG0_START	UINT32_C(0x80000000)
+#define KM_MIPS32_KSEG0_SIZE	UINT32_C(0x20000000)
+
+#define KM_MIPS32_KSSEG_START	UINT32_C(0xc0000000)
+#define KM_MIPS32_KSSEG_SIZE	UINT32_C(0x20000000)
+
+#define KM_MIPS32_KSEG3_START	UINT32_C(0xe0000000)
+#define KM_MIPS32_KSEG3_SIZE	UINT32_C(0x20000000)
+
 extern void km_identity_arch_init(void);
 extern void km_non_identity_arch_init(void);
 
--- kernel/arch/mips32/src/mm/km.c
+++ kernel/arch/mips32/src/mm/km.c
@@ -31,13 +31,19 @@
  */
 
 #include <arch/mm/km.h>
+#include <mm/km.h>
+#include <config.h>
 
 void km_identity_arch_init(void)
 {
+	config.identity_base = KM_MIPS32_KSEG0_START;
+	config.identity_size = KM_MIPS32_KSEG0_SIZE;
 }
 
 void km_non_identity_arch_init(void)
 {
+	km_non_identity_span_add(KM_MIPS32_KSSEG_START, KM_MIPS32_KSSEG_SIZE);
+	km_non_identity_span_add(KM_MIPS32_KSEG0_START, KM_MIPS32_KSEG0_SIZE);
 }
 
 /** @}
