e1ed831f96ab81a3ef1141d499a59bf8eb46eee3
Jakub Jermar <jakub@jermar.eu>
2011-11-09 00:06:08 +0100

    Partition ia64 kernel memory.

--- kernel/arch/ia64/include/mm/km.h
+++ kernel/arch/ia64/include/mm/km.h
@@ -35,6 +35,18 @@
 #ifndef KERN_ia64_KM_H_
 #define KERN_ia64_KM_H_
 
+/*
+ * Be conservative and assume the minimal (3 + 51)-bit virtual address width
+ * of the Itanium CPU even if running on CPU with full 64-bit virtual
+ * address width, such as Itanium 2.
+ */
+
+#define KM_IA64_IDENTITY_START		UINT64_C(0xe000000000000000)
+#define KM_IA64_IDENTITY_SIZE		UINT64_C(0x0004000000000000)
+
+#define KM_IA64_NON_IDENTITY_START	UINT64_C(0xfffc000000000000)
+#define KM_IA64_NON_IDENTITY_SIZE	UINT64_C(0x0004000000000000)
+
 extern void km_identity_arch_init(void);
 extern void km_non_identity_arch_init(void);
 
