2f80b866a195d42afdd303ad12934281dcc1a897
Jan Vesely <jano.vesely@gmail.com>
2011-12-06 16:09:31 +0100

    libusbdev: Add bitfield interpretation.

--- uspace/lib/usbdev/include/usb/dev/request.h
+++ uspace/lib/usbdev/include/usb/dev/request.h
@@ -82,6 +82,15 @@ typedef struct {
 	 */
 	uint8_t request_type;
 #define SETUP_REQUEST_TYPE_DEVICE_TO_HOST (1 << 7)
+#define SETUP_REQUEST_TYPE_GET_TYPE(rt) ((rt >> 5) & 0x3)
+#define SETUP_REQUEST_TYPE_STANDART 0
+#define SETUP_REQUEST_TYPE_CLASS    1
+#define SETUP_REQUEST_TYPE_VENDOR   2
+#define SETUP_REQUEST_TYPE_GET_RECIPIENT(rt) (rt & 0x1f)
+#define SETUP_REQUEST_TYPE_REC_DEVICE    0
+#define SETUP_REQUEST_TYPE_REC_INTERFACE 1
+#define SETUP_REQUEST_TYPE_REC_ENDPOINT  2
+#define SETUP_REQUEST_TYPE_REC_OTHER     3
 
 	/** Request identification. */
 	uint8_t request;
