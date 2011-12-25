65ceb4b0b11afe7b38fe481dece020ddf973cfe3
Jan Vesely <jano.vesely@gmail.com>
2011-12-26 00:29:43 +0100

    xtkbd: Add default connection handler and add device to keyboard category.

--- uspace/drv/char/xtkbd/xtkbd.c
+++ uspace/drv/char/xtkbd/xtkbd.c
@@ -36,10 +36,18 @@
 #include <devman.h>
 #include <device/char_dev.h>
 #include <ddf/log.h>
+#include <ipc/kbdev.h>
+#include <abi/ipc/methods.h>
 
 #include "xtkbd.h"
 
 static int polling(void *);
+static void default_connection_handler(ddf_fun_t *fun,
+    ipc_callid_t icallid, ipc_call_t *icall);
+
+static ddf_dev_ops_t kbd_ops = {
+	.default_handler = default_connection_handler
+};
 
 int xt_kbd_init(xt_kbd_t *kbd, ddf_dev_t *dev)
 {
@@ -56,10 +64,22 @@ int xt_kbd_init(xt_kbd_t *kbd, ddf_dev_t *dev)
 		async_hangup(kbd->parent_sess);
 		return ENOMEM;
 	}
+	kbd->kbd_fun->ops = &kbd_ops;
+	kbd->kbd_fun->driver_data = kbd;
 
-	const int ret = ddf_fun_bind(kbd->kbd_fun);
+	int ret = ddf_fun_bind(kbd->kbd_fun);
 	if (ret != EOK) {
 		async_hangup(kbd->parent_sess);
+		kbd->kbd_fun->driver_data = NULL;
+		ddf_fun_destroy(kbd->kbd_fun);
+		return ENOMEM;
+	}
+
+	ret = ddf_fun_add_to_category(kbd->kbd_fun, "keyboard");
+	if (ret != EOK) {
+		async_hangup(kbd->parent_sess);
+		ddf_fun_unbind(kbd->kbd_fun);
+		kbd->kbd_fun->driver_data = NULL;
 		ddf_fun_destroy(kbd->kbd_fun);
 		return ENOMEM;
 	}
@@ -68,6 +88,7 @@ int xt_kbd_init(xt_kbd_t *kbd, ddf_dev_t *dev)
 	if (!kbd->polling_fibril) {
 		async_hangup(kbd->parent_sess);
 		ddf_fun_unbind(kbd->kbd_fun);
+		kbd->kbd_fun->driver_data = NULL;
 		ddf_fun_destroy(kbd->kbd_fun);
 		return ENOMEM;
 	}
@@ -85,5 +106,52 @@ int polling(void *arg)
 		uint8_t code = 0;
 		ssize_t size = char_dev_read(kbd->parent_sess, &code, 1);
 		ddf_msg(LVL_DEBUG, "Got scancode: %hhx, size: %zd", code, size);
+	}
+}
+/*----------------------------------------------------------------------------*/
+void default_connection_handler(ddf_fun_t *fun,
+    ipc_callid_t icallid, ipc_call_t *icall)
+{
+	if (fun == NULL || fun->driver_data == NULL) {
+		ddf_msg(LVL_ERROR, "%s: Missing parameter.\n", __FUNCTION__);
+		async_answer_0(icallid, EINVAL);
+		return;
+	}
+
+	const sysarg_t method = IPC_GET_IMETHOD(*icall);
+	xt_kbd_t *kbd = fun->driver_data;
+
+	switch (method) {
+	case KBDEV_SET_IND:
+		/* XT keyboards do not support setting mods */
+		async_answer_0(icallid, ENOTSUP);
+		break;
+	/* This might be ugly but async_callback_receive_start makes no
+	 * difference for incorrect call and malloc failure. */
+	case IPC_M_CONNECT_TO_ME: {
+		async_sess_t *sess =
+		    async_callback_receive_start(EXCHANGE_SERIALIZE, icall);
+		/* Probably ENOMEM error, try again. */
+		if (sess == NULL) {
+			ddf_msg(LVL_WARN,
+			    "Failed to create start input session");
+			async_answer_0(icallid, EAGAIN);
+			break;
+		}
+		if (kbd->input_sess == NULL) {
+			kbd->input_sess = sess;
+			ddf_msg(LVL_DEBUG, "Set input session");
+			async_answer_0(icallid, EOK);
+		} else {
+			ddf_msg(LVL_ERROR, "Input session already set");
+			async_answer_0(icallid, ELIMIT);
+		}
+		break;
+	}
+	default:
+			ddf_msg(LVL_ERROR,
+			    "Unknown method: %d.\n", (int)method);
+			async_answer_0(icallid, EINVAL);
+			break;
 	}
 }
