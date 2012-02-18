4d433fec783a346bec02712ffb6dac557a5479db
Maurizio Lombardi <m.lombardi85@gmail.com>
2012-02-18 11:23:23 +0100

    mkexfat: Fix copyright header

--- uspace/app/mkexfat/exfat.h
+++ uspace/app/mkexfat/exfat.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2010 Jiri Svoboda
- * Copyright (c) 2012 Maurizio Lombardi
+ * Copyright (c) 2008 Jakub Jermar
+ * Copyright (c) 2011 Oleg Romanenko
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
