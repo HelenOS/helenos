9e06e9fa2ad4e420ee2d58124b8efef7309293d6
Maurizio Lombardi <m.lombardi85@gmail.com>
2012-04-03 12:20:21 +0200

    rtc: add the month field to the tm structure

--- uspace/lib/c/include/sys/time.h
+++ uspace/lib/c/include/sys/time.h
@@ -50,6 +50,7 @@ struct tm {
 	int tm_min;       /* 0 - 59 */
 	int tm_hour;      /* 0 - 23 */
 	int tm_mday;      /* 1 - 31 */
+	int tm_mon;       /* 0 - 11 */
 	int tm_year;      /* years since 1900 */
 };
 
