296ef5d6c8734ef74a4d6df24a957f5cd4333397
Frantisek Princ <frantisek.princ@gmail.com>
2012-04-27 17:27:55 +0200

    Part of inode comments

--- uspace/lib/ext4/libext4_inode.c
+++ uspace/lib/ext4/libext4_inode.c
@@ -40,6 +40,11 @@
 #include <libblock.h>
 #include "libext4.h"
 
+/** Compute number of bits for block count.
+ *
+ *  @param block_size	filesystem block_size
+ *  @return		number of bits
+ */
 static uint32_t ext4_inode_block_bits_count(uint32_t block_size)
 {
 	uint32_t bits = 8;
@@ -53,6 +58,12 @@ static uint32_t ext4_inode_block_bits_count(uint32_t block_size)
 	return bits;
 }
 
+/** Get mode of the i-node.
+ *
+ * @param sb		superblock
+ * @param inode		i-node to load mode from
+ * @return 		mode of the i-node
+ */
 uint32_t ext4_inode_get_mode(ext4_superblock_t *sb, ext4_inode_t *inode)
 {
 	if (ext4_superblock_get_creator_os(sb) == EXT4_SUPERBLOCK_OS_HURD) {
@@ -62,6 +73,12 @@ uint32_t ext4_inode_get_mode(ext4_superblock_t *sb, ext4_inode_t *inode)
 	return uint16_t_le2host(inode->mode);
 }
 
+/** Set mode of the i-node.
+ *
+ * @param sb		superblock
+ * @param inode		i-node to set mode to
+ * @param mode		mode to set to i-node
+ */
 void ext4_inode_set_mode(ext4_superblock_t *sb, ext4_inode_t *inode, uint32_t mode)
 {
 	inode->mode = host2uint16_t_le((mode << 16) >> 16);
@@ -71,16 +88,32 @@ void ext4_inode_set_mode(ext4_superblock_t *sb, ext4_inode_t *inode, uint32_t mo
 	}
 }
 
+/** Get ID of the i-node owner (user id).
+ *
+ * @param inode		i-node to load uid from
+ * @return		user ID of the i-node owner
+ */
 uint32_t ext4_inode_get_uid(ext4_inode_t *inode)
 {
 	return uint32_t_le2host(inode->uid);
 }
 
+/** Set ID of the i-node owner.
+ *
+ * @param inode		i-node to set uid to
+ * @param uid		ID of the i-node owner
+ */
 void ext4_inode_set_uid(ext4_inode_t *inode, uint32_t uid)
 {
 	inode->uid = host2uint32_t_le(uid);
 }
 
+/** Get real i-node size.
+ *
+ * @param sb		superblock
+ * @param inode		i-node to load size from
+ * @return		real size of i-node
+ */
 uint64_t ext4_inode_get_size(ext4_superblock_t *sb, ext4_inode_t *inode)
 {
 	uint32_t major_rev = ext4_superblock_get_rev_level(sb);
@@ -92,9 +125,14 @@ uint64_t ext4_inode_get_size(ext4_superblock_t *sb, ext4_inode_t *inode)
 	return uint32_t_le2host(inode->size_lo);
 }
 
-void ext4_inode_set_size(ext4_inode_t *inode, uint64_t value) {
-	inode->size_lo = host2uint32_t_le((value << 32) >> 32);
-	inode->size_hi = host2uint32_t_le(value >> 32);
+/** Set real i-node size.
+ *
+ *  @param inode	inode to set size to
+ *  @param size		size of the i-node
+ */
+void ext4_inode_set_size(ext4_inode_t *inode, uint64_t size) {
+	inode->size_lo = host2uint32_t_le((size << 32) >> 32);
+	inode->size_hi = host2uint32_t_le(size >> 32);
 }
 
 uint32_t ext4_inode_get_access_time(ext4_inode_t *inode)
@@ -102,66 +140,110 @@ uint32_t ext4_inode_get_access_time(ext4_inode_t *inode)
 	return uint32_t_le2host(inode->access_time);
 }
 
+/** TODO comment
+ *
+ */
 void ext4_inode_set_access_time(ext4_inode_t *inode, uint32_t time)
 {
 	inode->access_time = host2uint32_t_le(time);
 }
 
+/** TODO comment
+ *
+ */
 uint32_t ext4_inode_get_change_inode_time(ext4_inode_t *inode)
 {
 	return uint32_t_le2host(inode->change_inode_time);
 }
 
+/** TODO comment
+ *
+ */
 void ext4_inode_set_change_inode_time(ext4_inode_t *inode, uint32_t time)
 {
 	inode->change_inode_time = host2uint32_t_le(time);
 }
 
+/** TODO comment
+ *
+ */
 uint32_t ext4_inode_get_modification_time(ext4_inode_t *inode)
 {
 	return uint32_t_le2host(inode->modification_time);
 }
 
+/** TODO comment
+ *
+ */
 void ext4_inode_set_modification_time(ext4_inode_t *inode, uint32_t time)
 {
 	inode->modification_time = host2uint32_t_le(time);
 }
 
+/** TODO comment
+ *
+ */
 uint32_t ext4_inode_get_deletion_time(ext4_inode_t *inode)
 {
 	return uint32_t_le2host(inode->deletion_time);
 }
 
+/** TODO comment
+ *
+ */
 void ext4_inode_set_deletion_time(ext4_inode_t *inode, uint32_t time)
 {
 	inode->deletion_time = host2uint32_t_le(time);
 }
 
+/** Get ID of the i-node owner's group.
+ *
+ * @param inode         i-node to load gid from
+ * @return              group ID of the i-node owner
+ */
 uint32_t ext4_inode_get_gid(ext4_inode_t *inode)
 {
 	return uint32_t_le2host(inode->gid);
 }
 
+/** Set ID ot the i-node owner's group.
+ *
+ * @param inode		i-node to set gid to
+ * @param gid		group ID of the i-node owner
+ */
 void ext4_inode_set_gid(ext4_inode_t *inode, uint32_t gid)
 {
 	inode->gid = host2uint32_t_le(gid);
 }
 
+/** Get number of links to i-node.
+ *
+ * @param inode 	i-node to load number of links from
+ * @return		number of links to i-node
+ */
 uint16_t ext4_inode_get_links_count(ext4_inode_t *inode)
 {
 	return uint16_t_le2host(inode->links_count);
 }
 
+/** Set number of links to i-node.
+ *
+ * @param inode 	i-node to set number of links to
+ * @param count		number of links to i-node
+ */
 void ext4_inode_set_links_count(ext4_inode_t *inode, uint16_t count)
 {
 	inode->links_count = host2uint16_t_le(count);
 }
 
+/** TODO comment
+ *
+ */
 uint64_t ext4_inode_get_blocks_count(ext4_superblock_t *sb, ext4_inode_t *inode)
 {
 	if (ext4_superblock_has_feature_read_only(sb, EXT4_FEATURE_RO_COMPAT_HUGE_FILE)) {
 
-		/* 48-bit field */
+		// 48-bit field
 		uint64_t count = ((uint64_t)uint16_t_le2host(inode->osd2.linux2.blocks_high)) << 32 |
 				uint32_t_le2host(inode->blocks_count_lo);
 
@@ -178,6 +260,10 @@ uint64_t ext4_inode_get_blocks_count(ext4_superblock_t *sb, ext4_inode_t *inode)
 
 }
 
+
+/** TODO comment
+ *
+ */
 int ext4_inode_set_blocks_count(ext4_superblock_t *sb, ext4_inode_t *inode,
 		uint64_t count)
 {
@@ -215,24 +301,43 @@ int ext4_inode_set_blocks_count(ext4_superblock_t *sb, ext4_inode_t *inode,
     return EOK;
 }
 
+/** Get flags (features) of i-node.
+ * 
+ * @param inode		i-node to get flags from
+ * @return		flags (bitmap)
+ */
 uint32_t ext4_inode_get_flags(ext4_inode_t *inode) {
 	return uint32_t_le2host(inode->flags);
 }
 
+/** Set flags (features) of i-node.
+ *
+ * @param inode		i-node to set flags to
+ * @param flags		flags to set to i-node
+ */
 void ext4_inode_set_flags(ext4_inode_t *inode, uint32_t flags) {
 	inode->flags = host2uint32_t_le(flags);
 }
 
+/** TODO comment
+ *
+ */
 uint32_t ext4_inode_get_generation(ext4_inode_t *inode)
 {
 	return uint32_t_le2host(inode->generation);
 }
 
+/** TODO comment
+ *
+ */
 void ext4_inode_set_generation(ext4_inode_t *inode, uint32_t generation)
 {
 	inode->generation = host2uint32_t_le(generation);
 }
 
+/** TODO comment
+ *
+ */
 uint64_t ext4_inode_get_file_acl(ext4_inode_t *inode, ext4_superblock_t *sb)
 {
 	if (ext4_superblock_get_creator_os(sb) == EXT4_SUPERBLOCK_OS_LINUX) {
@@ -243,6 +348,9 @@ uint64_t ext4_inode_get_file_acl(ext4_inode_t *inode, ext4_superblock_t *sb)
 	return uint32_t_le2host(inode->file_acl_lo);
 }
 
+/** TODO comment
+ *
+ */
 void ext4_inode_set_file_acl(ext4_inode_t *inode, ext4_superblock_t *sb,
 		uint64_t file_acl)
 {
@@ -255,41 +363,81 @@ void ext4_inode_set_file_acl(ext4_inode_t *inode, ext4_superblock_t *sb,
 
 /***********************************************************************/
 
+/** Get block address of specified direct block.
+ *
+ * @param inode		i-node to load block from
+ * @param idx		index of logical block
+ * @return		physical block address
+ */
 uint32_t ext4_inode_get_direct_block(ext4_inode_t *inode, uint32_t idx)
 {
 	assert(idx < EXT4_INODE_DIRECT_BLOCK_COUNT);
 	return uint32_t_le2host(inode->blocks[idx]);
 }
 
+/** Set block address of specified direct block.
+ *
+ * @param inode		i-node to set block address to
+ * @param idx		index of logical block
+ * @param fblock	physical block address
+ */
 void ext4_inode_set_direct_block(ext4_inode_t *inode, uint32_t idx, uint32_t fblock)
 {
 	assert(idx < EXT4_INODE_DIRECT_BLOCK_COUNT);
 	inode->blocks[idx] = host2uint32_t_le(fblock);
 }
 
+/** Get block address of specified indirect block.
+ *
+ * @param inode		i-node to get block address from
+ * @param idx		index of indirect block 
+ * @return		physical block address
+ */
 uint32_t ext4_inode_get_indirect_block(ext4_inode_t *inode, uint32_t idx)
 {
 	return uint32_t_le2host(inode->blocks[idx + EXT4_INODE_INDIRECT_BLOCK]);
 }
 
+/** Set block address of specified indirect block.
+ *
+ * @param inode		i-node to set block address to
+ * @param idx		index of indirect block
+ * @param fblock	physical block address
+ */
 void ext4_inode_set_indirect_block(ext4_inode_t *inode, uint32_t idx, uint32_t fblock)
 {
 	inode->blocks[idx + EXT4_INODE_INDIRECT_BLOCK] = host2uint32_t_le(fblock);
 }
 
+/** Check if i-node has specified type.
+ *
+ * @param sb		superblock
+ * @param inode		i-node to check type of
+ * @param type		type to check
+ * @return 		result of check operation
+ */
 bool ext4_inode_is_type(ext4_superblock_t *sb, ext4_inode_t *inode, uint32_t type)
 {
 	uint32_t mode = ext4_inode_get_mode(sb, inode);
 	return (mode & EXT4_INODE_MODE_TYPE_MASK) == type;
 }
 
-
+/** Get extent header from the root of the extent tree.
+ *
+ * @param inode		i-node to get extent header from
+ * @return		pointer to extent header of the root node
+ */
 ext4_extent_header_t * ext4_inode_get_extent_header(ext4_inode_t *inode)
 {
 	return (ext4_extent_header_t *)inode->blocks;
 }
 
-// Flags operations
+/** Check if i-node has specified flag.
+ *
+ * @param inode		i-node to check flags of
+ * @param flag		flag to check
+ * @return		result of check operation
+ */
 bool ext4_inode_has_flag(ext4_inode_t *inode, uint32_t flag)
 {
 	if (ext4_inode_get_flags(inode) & flag) {
@@ -298,6 +446,11 @@ bool ext4_inode_has_flag(ext4_inode_t *inode, uint32_t flag)
 	return false;
 }
 
+/** Remove specified flag from i-node.
+ *
+ * @param inode		i-node to clear flag on
+ * @param clear_flag	flag to be cleared
+ */
 void ext4_inode_clear_flag(ext4_inode_t *inode, uint32_t clear_flag)
 {
 	uint32_t flags = ext4_inode_get_flags(inode);
@@ -305,6 +458,11 @@ void ext4_inode_clear_flag(ext4_inode_t *inode, uint32_t clear_flag)
 	ext4_inode_set_flags(inode, flags);
 }
 
+/** Set specified flag to i-node.
+ *
+ * @param inode		i-node to set flag on
+ * @param set_flag	falt to be set
+ */
 void ext4_inode_set_flag(ext4_inode_t *inode, uint32_t set_flag)
 {
 	uint32_t flags = ext4_inode_get_flags(inode);
@@ -312,6 +470,12 @@ void ext4_inode_set_flag(ext4_inode_t *inode, uint32_t set_flag)
 	ext4_inode_set_flags(inode, flags);
 }
 
+/** Check if i-node can be truncated.
+ *
+ * @param sb		superblock
+ * @param inode		i-node to check
+ * @return 		result of the check operation
+ */
 bool ext4_inode_can_truncate(ext4_superblock_t *sb, ext4_inode_t *inode)
 {
 	 if (ext4_inode_has_flag(inode, EXT4_INODE_FLAG_APPEND)
