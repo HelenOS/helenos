e0c80f665c3acae1820d84306689330477ca7c75
Adam Hraska <adam.hraska+hos@gmail.com>
2012-07-10 19:40:28 +0200

    smp_call: Added license.

--- kernel/generic/src/smp/smp_call.c
+++ kernel/generic/src/smp/smp_call.c
@@ -1,8 +1,44 @@
+/*
+ * Copyright (c) 2012 Adam Hraska
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @addtogroup generic
+ * @{
+ */
+
+/**
+ * @file
+ * @brief Facility to invoke functions on other cpus via IPIs.
+ */
+
 #include <smp/smp_call.h>
 #include <arch.h>
 #include <config.h>
 #include <preemption.h>
-#include <compiler/barrier.h>
 #include <arch/barrier.h>
 #include <arch/asm.h>  /* interrupt_disable */
 
@@ -12,6 +48,7 @@ static void call_done(smp_call_t *call_info);
 static void call_wait(smp_call_t *call_info);
 
 
+/** Init smp_call() on the local cpu. */
 void smp_call_init(void)
 {
 	ASSERT(CPU);
@@ -209,4 +246,5 @@ static void call_wait(smp_call_t *call_info)
 }
 
 
-
+/** @}
+ */
