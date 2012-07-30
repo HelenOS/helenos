1772e6d8daa54fce8e571f38accabed8f8303558
Martin Sucha <sucha14@st.fmph.uniba.sk>
2012-07-30 23:34:04 +0200

    Update documentation for str_cmp and str_lcmp.
    This is a modified version of formulation suggested by Jiri Zarevucky.

--- boot/generic/src/str.c
+++ boot/generic/src/str.c
@@ -356,15 +356,16 @@ bool chr_check(wchar_t ch)
  * The strings are considered equal iff their length is equal
  * and both strings consist of the same sequence of characters.
  *
- * A string is smaller than another string iff it is shorter or
- * has a character with lower value at the first position where
- * the strings differ.
+ * A string S1 is less than another string S2 if it has a character with
+ * lower value at the first character position where the strings differ.
+ * If the strings differ in length, the shorter one is treated as if
+ * padded by characters with a value of zero.
  *
  * @param s1 First string to compare.
  * @param s2 Second string to compare.
  *
- * @return 0 if the strings are equal, -1 if first is smaller,
- *         1 if second smaller.
+ * @return 0 if the strings are equal, -1 if the first is less than the second,
+ *         1 if the second is less than the first.
  *
  */
 int str_cmp(const char *s1, const char *s2)
