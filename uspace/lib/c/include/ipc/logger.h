bf9ac4e8bd9ebe9b2a11cce61848bb60ffa8d492
Vojtech Horky <vojtechhorky@users.sourceforge.net>
2012-08-17 12:08:21 +0200

    Add comments to logger IPC

--- uspace/lib/c/include/ipc/logger.h
+++ uspace/lib/c/include/ipc/logger.h
@@ -36,13 +36,35 @@
 #include <ipc/common.h>
 
 typedef enum {
+	/** Set (global) default displayed logging level.
+	 *
+	 * Arguments: new log level.
+	 * Returns: error code
+	 */
 	LOGGER_CONTROL_SET_DEFAULT_LEVEL = IPC_FIRST_USER_METHOD,
+	/** Set displayed level for given log.
+	 *
+	 * Arguments: new log level.
+	 * Returns: error code
+	 * Followed by: string with full log name.
+	 */
 	LOGGER_CONTROL_SET_LOG_LEVEL
 } logger_control_request_t;
 
 typedef enum {
-	/** Followed by STRING, returns ID to be used for CREATE_SUB_LOG */
+	/** Create new log.
+	 *
+	 * Arguments: parent log id (0 for top-level log).
+	 * Returns: error code, log id
+	 * Followed by: string with log name.
+	 */
 	LOGGER_WRITER_CREATE_LOG = IPC_FIRST_USER_METHOD,
+	/** Write a message to a given log.
+	 *
+	 * Arguments: log id, message severity level (log_level_t)
+	 * Returns: error code
+	 * Followed by: string with the message.
+	 */
 	LOGGER_WRITER_MESSAGE
 } logger_writer_request_t;
 
