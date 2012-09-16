8930624cad0624f7f999680a16864740e48e6484
Maurizio Lombardi <m.lombardi85@gmail.com>
2012-09-16 13:19:44 +0200

    Add the remote_battery_dev_iface to the remote interfaces list

--- uspace/lib/drv/generic/dev_iface.c
+++ uspace/lib/drv/generic/dev_iface.c
@@ -41,6 +41,7 @@
 #include "remote_hw_res.h"
 #include "remote_char_dev.h"
 #include "remote_clock_dev.h"
+#include "remote_battery_dev.h"
 #include "remote_graph_dev.h"
 #include "remote_nic.h"
 #include "remote_usb.h"
@@ -60,6 +61,7 @@ static iface_dipatch_table_t remote_ifaces = {
 		&remote_usbhc_iface,
 		&remote_usbhid_iface,
 		&remote_clock_dev_iface,
+		&remote_battery_dev_iface,
 		&remote_ahci_iface
 	}
 };
