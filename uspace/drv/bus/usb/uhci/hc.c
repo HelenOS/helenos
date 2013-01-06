3848feca7c69c1204d692083bc06624cce7aa7f1
Jan Vesely <jano.vesely@gmail.com>
2013-01-06 03:47:45 +0100

    uhci: Switch to new emulated root hub.

--- uspace/drv/bus/usb/uhci/hc.c
+++ uspace/drv/bus/usb/uhci/hc.c
@@ -408,8 +408,8 @@ int hc_schedule(hcd_t *hcd, usb_transfer_batch_t *batch)
 	assert(instance);
 	assert(batch);
 
-//	if (batch->ep->address == uhci_rh_get_address(&instance->rh))
-//		return uhci_rh_schedule(&instance->rh, batch);
+	if (batch->ep->address == uhci_rh_get_address(&instance->rh))
+		return uhci_rh_schedule(&instance->rh, batch);
 
 	uhci_transfer_batch_t *uhci_batch = uhci_transfer_batch_get(batch);
 	if (!uhci_batch) {
