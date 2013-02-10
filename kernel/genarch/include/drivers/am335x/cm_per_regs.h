9e3a345331d0159be6e940f013107deff98e0063
Maurizio Lombardi <m.lombardi85@gmail.com>
2013-02-10 18:25:27 +0100

    Fix copyright header

--- kernel/genarch/include/drivers/am335x/cm_per_regs.h
+++ kernel/genarch/include/drivers/am335x/cm_per_regs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Maurizio Lombardi
+ * Copyright (c) 2013 Maurizio Lombardi
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
