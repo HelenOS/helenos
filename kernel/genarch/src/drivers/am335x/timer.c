f09f059c73939b7b0e498d8b9ec73abb78a3ae7b
Maurizio Lombardi <m.lombardi85@gmail.com>
2013-02-13 23:10:05 +0100

    write the timer registers in posted mode

--- kernel/genarch/src/drivers/am335x/timer.c
+++ kernel/genarch/src/drivers/am335x/timer.c
@@ -37,6 +37,12 @@
 #include <mm/km.h>
 #include <errno.h>
 
+typedef enum {
+	REG_TCLR = 0x00,
+	REG_TCRR = 0x01,
+	REG_TLDR = 0x02,
+	REG_TTGR = 0x04
+} timer_reg_t;
 
 typedef struct timer_regs_mmap {
         uintptr_t base;
@@ -53,6 +59,28 @@ static const timer_regs_mmap_t regs_map[TIMERS_MAX] = {
 	{ .base = AM335x_DMTIMER6_BASE_ADDRESS, .size = AM335x_DMTIMER6_SIZE },
 	{ .base = AM335x_DMTIMER7_BASE_ADDRESS, .size = AM335x_DMTIMER7_SIZE },
 };
+
+static void
+write_register_posted(am335x_timer_t *timer, timer_reg_t reg, uint32_t value)
+{
+	am335x_timer_regs_t *regs = timer->regs;
+
+	while (regs->twps & reg);
+
+	switch (reg) {
+	default:
+		return;
+	case REG_TCLR:
+		regs->tclr = value;
+		break;
+	case REG_TCRR:
+		regs->tcrr = value;
+		break;
+	case REG_TLDR:
+		regs->tldr = value;
+		break;
+	}
+}
 
 void
 am335x_timer_init(am335x_timer_t *timer, am335x_timer_id_t id, unsigned hz,
@@ -74,24 +102,32 @@ am335x_timer_init(am335x_timer_t *timer, am335x_timer_id_t id, unsigned hz,
 
 	am335x_timer_regs_t *regs = timer->regs;
 
+	/* Enable the posted mode of operation */
+	regs->tsicr |= AM335x_TIMER_TSICR_POSTED_FLAG;
+
 	/* Stop the timer */
 	am335x_timer_stop(timer);
 
 	/* Perform a soft reset */
 	am335x_timer_reset(timer);
 
-	/* Disable the prescaler */
-	regs->tclr &= ~AM335x_TIMER_TCLR_PRE_FLAG;
+	unsigned tclr = regs->tclr;
 
+	/* Disable compare mode */
+	tclr &= ~AM335x_TIMER_TCLR_CE_FLAG;
+	/* Disable the prescaler */
+	tclr &= ~AM335x_TIMER_TCLR_PRE_FLAG;
 	/* Enable auto-reload mode */
-	regs->tclr |= AM335x_TIMER_TCLR_AR_FLAG;
+	tclr |= AM335x_TIMER_TCLR_AR_FLAG;
+
+	write_register_posted(timer, REG_TCLR, tclr);
 
 	/* Disable the emulation mode */
 	regs->tiocp_cfg |= AM335x_TIMER_TIOCPCFG_EMUFREE_FLAG;
 
-	unsigned const count = 0xFFFFFFFE - (srcclk_hz / hz);
-	regs->tcrr = count;
-	regs->tldr = count;
+	unsigned const count = 0xFFFFFFFF - (srcclk_hz / hz + 1);
+	write_register_posted(timer, REG_TCRR, count);
+	write_register_posted(timer, REG_TLDR, count);
 }
 
 void
@@ -115,8 +151,10 @@ am335x_timer_stop(am335x_timer_t *timer)
 {
 	/* Disable the interrupt */
 	timer->regs->irqenable_clr |= AM335x_TIMER_IRQENABLE_CLR_OVF_FLAG;
+	timer->regs->irqwakeen &= ~AM335x_TIMER_IRQWAKEEN_OVF_FLAG;
 	/* Stop the timer */
-	timer->regs->tclr &= ~AM335x_TIMER_TCLR_ST_FLAG;
+	write_register_posted(timer, REG_TCLR,
+	    timer->regs->tclr & ~AM335x_TIMER_TCLR_ST_FLAG);
 }
 
 void
@@ -124,8 +162,10 @@ am335x_timer_start(am335x_timer_t *timer)
 {
 	/* Enable the interrupt */
 	timer->regs->irqenable_set |= AM335x_TIMER_IRQENABLE_SET_OVF_FLAG;
+	timer->regs->irqwakeen |= AM335x_TIMER_IRQWAKEEN_OVF_FLAG;
 	/* Start the clock */
-	timer->regs->tclr |= AM335x_TIMER_TCLR_ST_FLAG;
+	write_register_posted(timer, REG_TCLR,
+	    timer->regs->tclr | AM335x_TIMER_TCLR_ST_FLAG);
 }
 
 /**
