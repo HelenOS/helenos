a269d05ba15fa23f6a68542519bbc125f3993a60
Vojtech Horky <vojtechhorky@users.sourceforge.net>
2013-05-03 15:37:30 +0200

    Add __popcountsi2 as a builtin

--- uspace/lib/c/generic/bitops.c
+++ uspace/lib/c/generic/bitops.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006 Ondrej Palkovsky
+ * Copyright (c) 2013 Vojtech Horky
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,89 +26,17 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/** @addtogroup generic
+/** @addtogroup libc
  * @{
  */
-/** @file
- */
 
-#ifndef LIBC_BITOPS_H_
-#define LIBC_BITOPS_H_
+#include <bitops.h>
 
-#include <sys/types.h>
-
-/** Mask with bit @a n set. */
-#define BIT_V(type, n) \
-    ((type) 1 << (n))
-
-/** Mask with rightmost @a n bits set. */
-#define BIT_RRANGE(type, n) \
-    (BIT_V(type, (n)) - 1)
-
-/** Mask with bits @a hi .. @a lo set. @a hi >= @a lo. */
-#define BIT_RANGE(type, hi, lo) \
-    (BIT_RRANGE(type, (hi) - (lo) + 1) << (lo))
-
-/** Extract range of bits @a hi .. @a lo from @a value. */
-#define BIT_RANGE_EXTRACT(type, hi, lo, value) \
-    (((value) >> (lo)) & BIT_RRANGE(type, (hi) - (lo) + 1))
-
-/** Return position of first non-zero bit from left (i.e. [log_2(arg)]).
- *
- * If number is zero, it returns 0
- */
-static inline unsigned int fnzb32(uint32_t arg)
+extern int __popcountsi2(int a)
 {
-	unsigned int n = 0;
-	
-	if (arg >> 16) {
-		arg >>= 16;
-		n += 16;
-	}
-	
-	if (arg >> 8) {
-		arg >>= 8;
-		n += 8;
-	}
-	
-	if (arg >> 4) {
-		arg >>= 4;
-		n += 4;
-	}
-	
-	if (arg >> 2) {
-		arg >>= 2;
-		n += 2;
-	}
-	
-	if (arg >> 1) {
-		arg >>= 1;
-		n += 1;
-	}
-	
-	return n;
+	return __builtin_popcount(a);
 }
 
-static inline unsigned int fnzb64(uint64_t arg)
-{
-	unsigned int n = 0;
-	
-	if (arg >> 32) {
-		arg >>= 32;
-		n += 32;
-	}
-	
-	return (n + fnzb32((uint32_t) arg));
-}
-
-static inline unsigned int fnzb(size_t arg)
-{
-	return fnzb64(arg);
-}
-
-extern int __popcountsi2(int);
-
-#endif
 
 /** @}
  */
