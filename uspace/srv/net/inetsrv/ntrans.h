08ed1377c1cf36aaa5a0cc9311ce8b841365d06e
Martin Decky <martin@decky.cz>
2013-07-12 17:26:30 +0200

    basic NDP implementation

---
+++ uspace/srv/net/inetsrv/ntrans.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2013 Antonin Steinhauser
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @addtogroup inet
+ * @{
+ */
+/**
+ * @file
+ * @brief
+ */
+
+#ifndef NTRANS_H_
+#define NTRANS_H_
+
+#include <inet/iplink_srv.h>
+#include <inet/addr.h>
+
+/** Address translation table element */
+typedef struct {
+	link_t ntrans_list;
+	addr128_t ip_addr;
+	addr48_t mac_addr;
+} inet_ntrans_t;
+
+extern int ntrans_add(addr128_t, addr48_t);
+extern int ntrans_remove(addr128_t);
+extern int ntrans_lookup(addr128_t, addr48_t);
+extern int ntrans_wait_timeout(suseconds_t);
+
+#endif
+
+/** @}
+ */
