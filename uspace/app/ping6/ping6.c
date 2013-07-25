ccdc63e559f32b50f2381110a99211f6ed0775f3
Martin Decky <martin@decky.cz>
2013-07-25 21:35:54 +0200

    handle timeouts properly in the repeated (-r) ping mode (this fixes #457) add the -n option based on the original patch by Jakub Klama

--- uspace/app/ping6/ping6.c
+++ uspace/app/ping6/ping6.c
@@ -41,8 +41,11 @@
 #include <inet/addr.h>
 #include <inet/inetping6.h>
 #include <io/console.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <str.h>
+#include <str_error.h>
 #include <sys/types.h>
 
 #define NAME "ping6"
@@ -53,11 +56,21 @@
 /** Ping request timeout in microseconds */
 #define PING_TIMEOUT (1000 * 1000)
 
-static int ping_ev_recv(inetping6_sdu_t *);
+typedef enum {
+	RECEIVED_NONE,
+	RECEIVED_SUCCESS,
+	RECEIVED_INTERRUPT
+} received_t;
 
-static bool done = false;
-static FIBRIL_CONDVAR_INITIALIZE(done_cv);
-static FIBRIL_MUTEX_INITIALIZE(done_lock);
+static received_t received;
+static FIBRIL_CONDVAR_INITIALIZE(received_cv);
+static FIBRIL_MUTEX_INITIALIZE(received_lock);
+
+static bool quit = false;
+static FIBRIL_CONDVAR_INITIALIZE(quit_cv);
+static FIBRIL_MUTEX_INITIALIZE(quit_lock);
+
+static int ping_ev_recv(inetping6_sdu_t *);
 
 static inetping6_ev_ops_t ev_ops = {
 	.recv = ping_ev_recv
@@ -66,19 +79,30 @@ static inetping6_ev_ops_t ev_ops = {
 static addr128_t src;
 static addr128_t dest;
 
-static bool ping_repeat = false;
+static bool repeat_forever = false;
+static size_t repeat_count = 1;
+
+static const char *short_options = "rn:";
 
 static void print_syntax(void)
 {
-	printf("syntax: " NAME " [-r] <host>\n");
+	printf("Syntax: %s [-n <count>|-r] <host>\n", NAME);
 }
 
-static void ping_signal_done(void)
+static void ping_signal_received(received_t value)
 {
-	fibril_mutex_lock(&done_lock);
-	done = true;
-	fibril_mutex_unlock(&done_lock);
-	fibril_condvar_broadcast(&done_cv);
+	fibril_mutex_lock(&received_lock);
+	received = value;
+	fibril_mutex_unlock(&received_lock);
+	fibril_condvar_broadcast(&received_cv);
+}
+
+static void ping_signal_quit(void)
+{
+	fibril_mutex_lock(&quit_lock);
+	quit = true;
+	fibril_mutex_unlock(&quit_lock);
+	fibril_condvar_broadcast(&quit_cv);
 }
 
 static int ping_ev_recv(inetping6_sdu_t *sdu)
@@ -104,8 +128,7 @@ static int ping_ev_recv(inetping6_sdu_t *sdu)
 	printf("Received ICMPv6 echo reply: from %s to %s, seq. no %u, "
 	    "payload size %zu\n", asrc, adest, sdu->seq_no, sdu->size);
 	
-	if (!ping_repeat)
-		ping_signal_done();
+	ping_signal_received(RECEIVED_SUCCESS);
 	
 	free(asrc);
 	free(adest);
@@ -115,65 +138,80 @@ static int ping_ev_recv(inetping6_sdu_t *sdu)
 static int ping_send(uint16_t seq_no)
 {
 	inetping6_sdu_t sdu;
-	int rc;
-
+	
 	addr128(src, sdu.src);
 	addr128(dest, sdu.dest);
 	sdu.seq_no = seq_no;
 	sdu.data = (void *) "foo";
 	sdu.size = 3;
-
-	rc = inetping6_send(&sdu);
-	if (rc != EOK) {
-		printf(NAME ": Failed sending echo request (%d).\n", rc);
-		return rc;
-	}
-
-	return EOK;
+	
+	int rc = inetping6_send(&sdu);
+	if (rc != EOK)
+		printf("Failed sending echo request: %s (%d).\n",
+		    str_error(rc), rc);
+	
+	return rc;
 }
 
 static int transmit_fibril(void *arg)
 {
 	uint16_t seq_no = 0;
-
-	while (true) {
-		fibril_mutex_lock(&done_lock);
-		if (done) {
-			fibril_mutex_unlock(&done_lock);
-			return 0;
-		}
-		fibril_mutex_unlock(&done_lock);
-
+	
+	while ((repeat_count--) || (repeat_forever)) {
+		fibril_mutex_lock(&received_lock);
+		received = RECEIVED_NONE;
+		fibril_mutex_unlock(&received_lock);
+		
 		(void) ping_send(++seq_no);
-		async_usleep(PING_DELAY);
+		
+		fibril_mutex_lock(&received_lock);
+		int rc = fibril_condvar_wait_timeout(&received_cv, &received_lock,
+		    PING_TIMEOUT);
+		received_t recv = received;
+		fibril_mutex_unlock(&received_lock);
+		
+		if ((rc == ETIMEOUT) || (recv == RECEIVED_NONE))
+			printf("Echo request timed out (seq. no %u)\n", seq_no);
+		
+		if (recv == RECEIVED_INTERRUPT)
+			break;
+		
+		if ((repeat_count > 0) || (repeat_forever)) {
+			fibril_mutex_lock(&received_lock);
+			rc = fibril_condvar_wait_timeout(&received_cv, &received_lock,
+			    PING_DELAY);
+			recv = received;
+			fibril_mutex_unlock(&received_lock);
+			
+			if (recv == RECEIVED_INTERRUPT)
+				break;
+		}
 	}
-
+	
+	ping_signal_quit();
 	return 0;
 }
 
 static int input_fibril(void *arg)
 {
-	console_ctrl_t *con;
-	cons_event_t ev;
-
-	con = console_init(stdin, stdout);
-	printf("[Press Ctrl-Q to quit]\n");
-
+	console_ctrl_t *con = console_init(stdin, stdout);
+	
 	while (true) {
+		cons_event_t ev;
 		if (!console_get_event(con, &ev))
 			break;
-
-		if (ev.type == CEV_KEY && ev.ev.key.type == KEY_PRESS &&
-		    (ev.ev.key.mods & (KM_ALT | KM_SHIFT)) ==
-		    0 && (ev.ev.key.mods & KM_CTRL) != 0) {
+		
+		if ((ev.type == CEV_KEY) && (ev.ev.key.type == KEY_PRESS) &&
+		    ((ev.ev.key.mods & (KM_ALT | KM_SHIFT)) == 0) &&
+		    ((ev.ev.key.mods & KM_CTRL) != 0)) {
 			/* Ctrl+key */
 			if (ev.ev.key.key == KC_Q) {
-				ping_signal_done();
-				return 0;
+				ping_signal_received(RECEIVED_INTERRUPT);
+				break;
 			}
 		}
 	}
-
+	
 	return 0;
 }
 
@@ -183,37 +221,49 @@ int main(int argc, char *argv[])
 	char *asrc = NULL;
 	char *adest = NULL;
 	char *sdest = NULL;
-	int rc;
-	int argi;
-
-	rc = inetping6_init(&ev_ops);
+	
+	int rc = inetping6_init(&ev_ops);
 	if (rc != EOK) {
-		printf(NAME ": Failed connecting to internet ping service "
-		    "(%d).\n", rc);
+		printf("Failed connecting to internet ping service: "
+		    "%s (%d).\n", str_error(rc), rc);
 		goto error;
 	}
-
-	argi = 1;
-	if (argi < argc && str_cmp(argv[argi], "-r") == 0) {
-		ping_repeat = true;
-		++argi;
-	} else {
-		ping_repeat = false;
+	
+	int c;
+	while ((c = getopt(argc, argv, short_options)) != -1) {
+		switch (c) {
+		case 'r':
+			repeat_forever = true;
+			break;
+		case 'n':
+			rc = str_size_t(optarg, NULL, 10, true, &repeat_count);
+			if (rc != EOK) {
+				printf("Invalid repeat count.\n");
+				print_syntax();
+				goto error;
+			}
+			break;
+		default:
+			printf("Unknown option passed.\n");
+			print_syntax();
+			goto error;
+		}
 	}
-
-	if (argc - argi != 1) {
+	
+	if (optind >= argc) {
+		printf("IP address or host name not supplied.\n");
 		print_syntax();
 		goto error;
 	}
-
+	
 	/* Parse destination address */
 	inet_addr_t dest_addr;
-	rc = inet_addr_parse(argv[argi], &dest_addr);
+	rc = inet_addr_parse(argv[optind], &dest_addr);
 	if (rc != EOK) {
 		/* Try interpreting as a host name */
-		rc = dnsr_name2host(argv[argi], &hinfo, AF_INET6);
+		rc = dnsr_name2host(argv[optind], &hinfo, AF_INET);
 		if (rc != EOK) {
-			printf(NAME ": Error resolving host '%s'.\n", argv[argi]);
+			printf("Error resolving host '%s'.\n", argv[optind]);
 			goto error;
 		}
 		
@@ -222,15 +272,15 @@ int main(int argc, char *argv[])
 	
 	uint16_t af = inet_addr_get(&dest_addr, NULL, &dest);
 	if (af != AF_INET6) {
-		printf(NAME ": Destination '%s' is not an IPv6 address.\n",
-		    argv[argi]);
+		printf("Destination '%s' is not an IPv6 address.\n",
+		    argv[optind]);
 		goto error;
 	}
 	
 	/* Determine source address */
 	rc = inetping6_get_srcaddr(dest, src);
 	if (rc != EOK) {
-		printf(NAME ": Failed determining source address.\n");
+		printf("Failed determining source address.\n");
 		goto error;
 	}
 	
@@ -239,65 +289,51 @@ int main(int argc, char *argv[])
 	
 	rc = inet_addr_format(&src_addr, &asrc);
 	if (rc != EOK) {
-		printf(NAME ": Out of memory.\n");
+		printf("Out of memory.\n");
 		goto error;
 	}
 	
 	rc = inet_addr_format(&dest_addr, &adest);
 	if (rc != EOK) {
-		printf(NAME ": Out of memory.\n");
+		printf("Out of memory.\n");
 		goto error;
 	}
 	
 	if (hinfo != NULL) {
 		rc = asprintf(&sdest, "%s (%s)", hinfo->cname, adest);
 		if (rc < 0) {
-			printf(NAME ": Out of memory.\n");
+			printf("Out of memory.\n");
 			goto error;
 		}
 	} else {
 		sdest = adest;
 		adest = NULL;
 	}
-
-	printf("Sending ICMP echo request from %s to %s.\n",
+	
+	printf("Sending ICMP echo request from %s to %s (Ctrl+Q to quit)\n",
 	    asrc, sdest);
-
-	fid_t fid;
-
-	if (ping_repeat) {
-		fid = fibril_create(transmit_fibril, NULL);
-		if (fid == 0) {
-			printf(NAME ": Failed creating transmit fibril.\n");
-			goto error;
-		}
-
-		fibril_add_ready(fid);
-
-		fid = fibril_create(input_fibril, NULL);
-		if (fid == 0) {
-			printf(NAME ": Failed creating input fibril.\n");
-			goto error;
-		}
-
-		fibril_add_ready(fid);
-	} else {
-		ping_send(1);
-	}
-
-	fibril_mutex_lock(&done_lock);
-	rc = EOK;
-	while (!done && rc != ETIMEOUT) {
-		rc = fibril_condvar_wait_timeout(&done_cv, &done_lock,
-			ping_repeat ? 0 : PING_TIMEOUT);
-	}
-	fibril_mutex_unlock(&done_lock);
-
-	if (rc == ETIMEOUT) {
-		printf(NAME ": Echo request timed out.\n");
+	
+	fid_t fid = fibril_create(transmit_fibril, NULL);
+	if (fid == 0) {
+		printf("Failed creating transmit fibril.\n");
 		goto error;
 	}
-
+	
+	fibril_add_ready(fid);
+	
+	fid = fibril_create(input_fibril, NULL);
+	if (fid == 0) {
+		printf("Failed creating input fibril.\n");
+		goto error;
+	}
+	
+	fibril_add_ready(fid);
+	
+	fibril_mutex_lock(&quit_lock);
+	while (!quit)
+		fibril_condvar_wait(&quit_cv, &quit_lock);
+	fibril_mutex_unlock(&quit_lock);
+	
 	free(asrc);
 	free(adest);
 	free(sdest);
