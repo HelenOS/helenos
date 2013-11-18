3b0414362934a1f1fe258508a0f01fb01a42ad1e
Vojtech Horky <vojtechhorky@users.sourceforge.net>
2013-11-18 16:13:08 +0100

    Add long double constants to float.h

--- uspace/lib/posix/include/posix/float.h
+++ uspace/lib/posix/include/posix/float.h
@@ -68,6 +68,10 @@
 	#define FLT_EPSILON __FLT_EPSILON__
 	#undef FLT_MANT_DIG
 	#define FLT_MANT_DIG __FLT_MANT_DIG__
+	#undef LDBL_MIN
+	#define LDBL_MIN __LDBL_MIN__
+	#undef LDBL_MAX
+	#define LDBL_MAX __LDBL_MAX__
 	#undef LDBL_MANT_DIG 
 	#define LDBL_MANT_DIG __LDBL_MANT_DIG__
 #else
