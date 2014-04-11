eeb23f2d41d9507c41d2aed4028d94b834cfa126
Vojtech Horky <vojtechhorky@users.sourceforge.net>
2014-04-11 10:59:02 +0200

    Add some libposix tests

---
+++ uspace/lib/posix/test/scanf.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2014 Vojtech Horky
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define LIBPOSIX_INTERNAL
+#define __POSIX_DEF__(x) posix_##x
+
+#include "posix/errno.h"
+#include "posix/stdio.h"
+
+#include <pcut/test.h>
+
+
+PCUT_INIT
+
+PCUT_TEST_SUITE(scanf)
+
+
+PCUT_TEST(int_decimal) {
+	int number;
+	int rc = posix_sscanf("4242", "%d", &number);
+	PCUT_ASSERT_INT_EQUALS(1, rc);
+	PCUT_ASSERT_INT_EQUALS(4242, number);
+}
+
+PCUT_TEST(int_negative_decimal) {
+	int number;
+	int rc = posix_sscanf("-53", "%d", &number);
+	PCUT_ASSERT_INT_EQUALS(1, rc);
+	PCUT_ASSERT_INT_EQUALS(-53, number);
+}
+
+PCUT_EXPORT(scanf);
