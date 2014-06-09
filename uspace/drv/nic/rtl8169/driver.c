e37d2f81e32dc90959db7274d4ce71f44b8c1068
Agnieszka Tabaka <nufcia@gmail.com>
2014-06-09 23:34:34 +0200

    Remove _DDF_DATA_IMPLANT and ddf_fun_data_implant() call.

--- uspace/drv/nic/rtl8169/driver.c
+++ uspace/drv/nic/rtl8169/driver.c
@@ -27,8 +27,6 @@
  */
 
 /* XXX Fix this */
-#define _DDF_DATA_IMPLANT
-
 #include <assert.h>
 #include <errno.h>
 #include <align.h>
@@ -415,7 +413,6 @@ static int rtl8169_dev_add(ddf_dev_t *dev)
 
 	nic_set_ddf_fun(nic_data, fun);
 	ddf_fun_set_ops(fun, &rtl8169_dev_ops);
-	ddf_fun_data_implant(fun, nic_data);
 
 	rc = ddf_fun_bind(fun);
 	if (rc != EOK) {
