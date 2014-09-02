4dc7a849b2a316c7d909dd3603c70a012433c8b4
Jakub Jermar <jakub@jermar.eu>
2014-09-02 22:16:21 +0200

    Autogenerate amd64 fibril context_t and its offsets
    Generate the userspace context_t structure and its offsets by
    autogen.py.  Decouple the context definition from the kernel context_t,
    because these structures can, and in fact already do, have different
    members. Do not share the corresponding common assembly between the
    kernel and the userspace.

--- uspace/lib/c/arch/amd64/include/libarch/fibril.h
+++ uspace/lib/c/arch/amd64/include/libarch/fibril.h
@@ -35,7 +35,7 @@
 #ifndef LIBC_amd64_FIBRIL_H_
 #define LIBC_amd64_FIBRIL_H_
 
-#include <sys/types.h>
+#include <libarch/fibril_context.h>
 
 /*
  * According to ABI the stack MUST be aligned on
@@ -51,24 +51,6 @@
 		(c)->tls = (sysarg_t) (ptls); \
 		(c)->rbp = 0; \
 	} while (0)
-
-/* We include only registers that must be preserved
- * during function call
- */
-typedef struct {
-	uint64_t sp;
-	uint64_t pc;
-
-	uint64_t rbx;
-	uint64_t rbp;
-
-	uint64_t r12;
-	uint64_t r13;
-	uint64_t r14;
-	uint64_t r15;
-
-	uint64_t tls;
-} context_t;
 
 static inline uintptr_t context_get_fp(context_t *ctx)
 {
