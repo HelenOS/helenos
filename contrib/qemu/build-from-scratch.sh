61b208f646f3876ff110768890465a8c7d194886
Jakub Jermar <jakub@jermar.eu>
2017-01-22 14:22:55 +0100

    Drop support for building gem5
    - Move code for downloading the OpenSPARC binaries to
    qemu/build-from-scratch.sh.
    - Install the OpenSPARC binaries under /usr/local/opensparc/image
    when building QEMU

--- contrib/qemu/build-from-scratch.sh
+++ contrib/qemu/build-from-scratch.sh
@@ -37,6 +37,51 @@ URL=http://wiki.qemu-project.org/download/${TARBALL}
 REPO=git://git.qemu.org/qemu.git
 MD5="17940dce063b6ce450a12e719a6c9c43"
 
+OPENSPARC_TARBALL="OpenSPARCT1_Arch.1.5.tar.bz2"
+OPENSPARC_URL="http://download.oracle.com/technetwork/systems/opensparc/${OPENSPARC_TARBALL}"
+
+ARCHIVE_PREFIX="./S10image"
+BINARIES="1up-hv.bin 1up-md.bin nvram1 openboot.bin q.bin reset.bin"
+
+echo "==== Downloading OpenSPARC archive ===="
+
+if [ ! -f ${OPENSPARC_TARBALL} ]
+then
+    wget ${OPENSPARC_URL}
+else
+    echo "===== OpenSPARC archive already exists, skipping. ====="
+fi
+
+echo "==== Extracting OpenSPARC binaries ===="
+(
+    mkdir -p binaries;
+
+    BINLIST=""
+    for b in ${BINARIES};
+    do
+	if [ ! -f binaries/$b ];
+	then
+	    BINLIST+=${ARCHIVE_PREFIX}/$b" "
+	else
+	    echo "===== $b seems to be already extracted, skipping. ====="
+	fi
+    done
+
+    cd binaries
+
+    if [ "${BINLIST}x" != "x" ];
+    then
+	tar --strip-components=2 -xjf ../${OPENSPARC_TARBALL} ${BINLIST}
+    fi
+)
+
+echo "==== Installing OpenSPARC binaries ===="
+
+sudo install -d /usr/local/opensparc/image
+sudo install -m 0444 binaries/* /usr/local/opensparc/image
+
+echo "==== Obtaining QEMU sources ===="
+
 if [ "$1" == "--master" ]; then
 	git clone ${REPO} ${BASENAME_MASTER}
 	cd ${BASENAME_MASTER}
@@ -54,6 +99,15 @@ else
 	cd ${SOURCEDIR}
 fi
 
+echo "==== Configuring QEMU ===="
+
 ./configure --target-list=i386-softmmu,x86_64-softmmu,arm-softmmu,ppc-softmmu,sparc64-softmmu,mips-softmmu,mipsel-softmmu --audio-drv-list=pa
+
+echo "==== Building QEMU ===="
+
 make -j 4
+
+echo "==== Installing QEMU ===="
+
 sudo make install
+
