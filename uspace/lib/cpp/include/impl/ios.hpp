54939b27839da2d63a4ed8c17fdb67584070e701
Jaroslav Jindrak <dzejrou@gmail.com>
2017-11-04 18:57:49 +0100

    cpp: implemented ios_base manipulators

--- uspace/lib/cpp/include/impl/ios.hpp
+++ uspace/lib/cpp/include/impl/ios.hpp
@@ -546,6 +546,54 @@ namespace std
      * 27.5.6, ios_base manipulators:
      */
 
+    /**
+     * 27.5.6.1, fmtflags manipulators:
+     */
+
+    ios_base& boolalpha(ios_base& str);
+    ios_base& noboolalpha(ios_base& str);
+    ios_base& showbase(ios_base& str);
+    ios_base& noshowbase(ios_base& str);
+    ios_base& showpoint(ios_base& str);
+    ios_base& noshowpoint(ios_base& str);
+    ios_base& showpos(ios_base& str);
+    ios_base& noshowpos(ios_base& str);
+    ios_base& skipws(ios_base& str);
+    ios_base& noskipws(ios_base& str);
+    ios_base& uppercase(ios_base& str);
+    ios_base& nouppercase(ios_base& str);
+    ios_base& unitbuf(ios_base& str);
+    ios_base& nounitbuf(ios_base& str);
+
+    /**
+     * 27.5.6.2, adjustfield manipulators:
+     */
+
+    ios_base& internal(ios_base& str);
+    ios_base& left(ios_base& str);
+    ios_base& right(ios_base& str);
+
+    /**
+     * 27.5.6.3, basefield manupulators:
+     */
+
+    ios_base& dec(ios_base& str);
+    ios_base& hex(ios_base& str);
+    ios_base& oct(ios_base& str);
+
+    /**
+     * 27.5.6.4, floatfield manupulators:
+     */
+
+    ios_base& fixed(ios_base& str);
+    ios_base& scientific(ios_base& str);
+    ios_base& hexfloat(ios_base& str);
+    ios_base& defaultfloat(ios_base& str);
+
+    /**
+     * 27.5.6.5, error reporting:
+     */
+
     // TODO: implement
 }
 
--- uspace/lib/cpp/src/ios.cpp
+++ uspace/lib/cpp/src/ios.cpp
@@ -187,4 +187,148 @@ namespace std
     {
         callbacks.emplace_back(fn, index);
     }
+
+    ios_base& boolalpha(ios_base& str)
+    {
+        str.setf(ios_base::boolalpha);
+        return str;
+    }
+
+    ios_base& noboolalpha(ios_base& str)
+    {
+        str.unsetf(ios_base::boolalpha);
+        return str;
+    }
+
+    ios_base& showbase(ios_base& str)
+    {
+        str.setf(ios_base::showbase);
+        return str;
+    }
+
+    ios_base& noshowbase(ios_base& str)
+    {
+        str.unsetf(ios_base::showbase);
+        return str;
+    }
+
+    ios_base& showpoint(ios_base& str)
+    {
+        str.setf(ios_base::showpoint);
+        return str;
+    }
+
+    ios_base& noshowpoint(ios_base& str)
+    {
+        str.unsetf(ios_base::showpoint);
+        return str;
+    }
+
+    ios_base& showpos(ios_base& str)
+    {
+        str.setf(ios_base::showpos);
+        return str;
+    }
+
+    ios_base& noshowpos(ios_base& str)
+    {
+        str.unsetf(ios_base::showpos);
+        return str;
+    }
+
+    ios_base& skipws(ios_base& str)
+    {
+        str.setf(ios_base::skipws);
+        return str;
+    }
+
+    ios_base& noskipws(ios_base& str)
+    {
+        str.unsetf(ios_base::skipws);
+        return str;
+    }
+
+    ios_base& uppercase(ios_base& str)
+    {
+        str.setf(ios_base::uppercase);
+        return str;
+    }
+
+    ios_base& nouppercase(ios_base& str)
+    {
+        str.unsetf(ios_base::uppercase);
+        return str;
+    }
+
+    ios_base& unitbuf(ios_base& str)
+    {
+        str.setf(ios_base::unitbuf);
+        return str;
+    }
+
+    ios_base& nounitbuf(ios_base& str)
+    {
+        str.unsetf(ios_base::unitbuf);
+        return str;
+    }
+
+    ios_base& internal(ios_base& str)
+    {
+        str.setf(ios_base::internal, ios_base::adjustfield);
+        return str;
+    }
+
+    ios_base& left(ios_base& str)
+    {
+        str.setf(ios_base::left, ios_base::adjustfield);
+        return str;
+    }
+
+    ios_base& right(ios_base& str)
+    {
+        str.setf(ios_base::right, ios_base::adjustfield);
+        return str;
+    }
+
+    ios_base& dec(ios_base& str)
+    {
+        str.setf(ios_base::dec, ios_base::basefield);
+        return str;
+    }
+
+    ios_base& hex(ios_base& str)
+    {
+        str.setf(ios_base::hex, ios_base::basefield);
+        return str;
+    }
+
+    ios_base& oct(ios_base& str)
+    {
+        str.setf(ios_base::oct, ios_base::basefield);
+        return str;
+    }
+
+    ios_base& fixed(ios_base& str)
+    {
+        str.setf(ios_base::fixed, ios_base::floatfield);
+        return str;
+    }
+
+    ios_base& scientific(ios_base& str)
+    {
+        str.setf(ios_base::scientific, ios_base::floatfield);
+        return str;
+    }
+
+    ios_base& hexfloat(ios_base& str)
+    {
+        str.setf(ios_base::fixed | ios_base::scientific, ios_base::floatfield);
+        return str;
+    }
+
+    ios_base& defaultfloat(ios_base& str)
+    {
+        str.unsetf(ios_base::floatfield);
+        return str;
+    }
 }
