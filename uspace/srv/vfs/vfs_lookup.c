6fb8b2c38282012b1154d0e8fa18bbd84a81caec
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2017-12-06 22:54:37 +0100

    Use first return value from VFS_OUT_LOOKUP for file descriptor, instead of service id which is never changed.

--- uspace/srv/vfs/vfs_lookup.c
+++ uspace/srv/vfs/vfs_lookup.c
@@ -221,15 +221,15 @@ static int out_lookup(vfs_triplet_t *base, size_t *pfirst, size_t *plen,
 	async_wait_for(req, &rc);
 	vfs_exchange_release(exch);
 	
-	if ((int) rc < 0)
-		return (int) rc;
+	if (rc != EOK)
+		return rc;
 	
 	unsigned last = *pfirst + *plen;
 	*pfirst = IPC_GET_ARG3(answer) & 0xffff;
 	*plen = last - *pfirst;
 	
-	result->triplet.fs_handle = (fs_handle_t) rc;
-	result->triplet.service_id = (service_id_t) IPC_GET_ARG1(answer);
+	result->triplet.fs_handle = (fs_handle_t) IPC_GET_ARG1(answer);
+	result->triplet.service_id = base->service_id;
 	result->triplet.index = (fs_index_t) IPC_GET_ARG2(answer);
 	result->size = MERGE_LOUP32(IPC_GET_ARG4(answer), IPC_GET_ARG5(answer));
 	result->type = (IPC_GET_ARG3(answer) >> 16) ?
