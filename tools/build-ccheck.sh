623b21349224b9611d13854966f060a9008605c9
Jiri Svoboda <jiri@wiwaxia>
2018-01-14 19:20:16 +0100

    Bump sycek (enforce no indentation on blank lines)

--- tools/build-ccheck.sh
+++ tools/build-ccheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2018 Jiri Svoboda
+# Copyright (c) 2019 Jiri Svoboda
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -28,7 +28,7 @@
 #
 
 SYCEK_GIT="https://github.com/jxsvoboda/sycek"
-SYCEK_REV="42fe0d77819f0ec05f17e40ea54c7b62073c8e97"
+SYCEK_REV="6e335d8547faeb47626b62ca52dd8570495478cd"
 
 if [ ! -d sycek ]; then
 	git clone "$SYCEK_GIT" sycek
