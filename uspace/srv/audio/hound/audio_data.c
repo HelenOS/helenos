HelenOS/helenos

commit 33b8d024bc268f2773c26e74a40dada11bb71e58

Remove `const` qualifier from the argument of `free()` and `realloc()`, as well as in numerous other variables that hold ownership of memory.
By convention, a pointer that holds ownership is _never_ qualified by `const`.
This is reflected in the standard type signature of `free()` and `realloc()`.
Allowing const pointers to hold ownership may seem superficially convenient,
but is actually quite confusing to experienced C programmers.

uspace/srv/audio/hound/audio_data.c
@@ -35,6 +35,7 @@
 
 #include <macros.h>
 #include <stdlib.h>
+#include <str.h>
 
 #include "audio_data.h"
 #include "log.h"
@@ -49,14 +50,14 @@
 audio_data_t *audio_data_create(const void *data, size_t size,
     pcm_format_t format)
 {
-	audio_data_t *adata = malloc(sizeof(audio_data_t));
+	audio_data_t *adata = malloc(sizeof(audio_data_t) + size);
 	if (adata) {
 		unsigned overflow = size % pcm_format_frame_size(&format);
 		if (overflow)
 			log_warning("Data not a multiple of frame size, "
 			    "clipping.");
-
-		adata->data = data;
+		uint8_t *d = ((uint8_t *)adata) + offsetof(audio_data_t, data);
+		memcpy(d, data, size);
 		adata->size = size - overflow;
 		adata->format = format;
 		atomic_set(&adata->refcount, 1);
@@ -85,7 +86,6 @@ void audio_data_unref(audio_data_t *adata)
 	assert(atomic_get(&adata->refcount) > 0);
 	atomic_count_t refc = atomic_predec(&adata->refcount);
 	if (refc == 0) {
-		free(adata->data);
 		free(adata);
 	}
 }

uspace/srv/audio/hound/audio_data.h
@@ -44,14 +44,14 @@
 
 /** Reference counted audio buffer */
 typedef struct {
-	/** Audio data */
-	const void *data;
 	/** Size of the buffer pointer to by data */
 	size_t size;
 	/** Format of the audio data */
 	pcm_format_t format;
 	/** Reference counter */
 	atomic_t refcount;
+	/** Audio data */
+	const uint8_t data[];
 } audio_data_t;
 
 /** Audio data pipe structure */

uspace/app/bdsh/compl.c
@@ -68,9 +68,9 @@ typedef struct {
 	builtin_t *builtin;
 
 	/** Pointer inside list of directories */
-	const char **path;
+	const char *const *path;
 	/** If not @c NULL, should be freed in the end. */
-	const char **path_list;
+	char **path_list;
 	/** Current open directory */
 	DIR *dir;
 
@@ -218,7 +218,9 @@ static errno_t compl_init(wchar_t *text, size_t pos, size_t *cstart, void **stat
 		}
 		cs->path_list[0] = dirname;
 		cs->path_list[1] = NULL;
-		cs->path = cs->path_list;
+		/* The second const ensures that we can't assign a const
+		 * string to the non-const array. */
+		cs->path = (const char *const *) cs->path_list;
 
 	} else if (cs->is_command) {
 		/* Command without path */

uspace/app/edit/search_impl.h
@@ -42,7 +42,7 @@
 struct search {
 	/* Note: This structure is opaque for the user. */
 
-	const wchar_t *pattern;
+	wchar_t *pattern;
 	size_t pattern_length;
 	ssize_t *back_table;
 	size_t pattern_pos;

uspace/app/mixerctl/mixerctl.c
@@ -48,7 +48,7 @@
  */
 static void print_levels(async_exch_t *exch)
 {
-	const char* name = NULL;
+	char* name = NULL;
 	unsigned count = 0;
 	errno_t ret = audio_mixer_get_info(exch, &name, &count);
 	if (ret != EOK) {
@@ -58,7 +58,7 @@ static void print_levels(async_exch_t *exch)
 	printf("MIXER %s:\n\n", name);
 
 	for (unsigned i = 0; i < count; ++i) {
-		const char *name = NULL;
+		char *name = NULL;
 		unsigned levels = 0, current = 0;
 		errno_t ret =
 		    audio_mixer_get_item_info(exch, i, &name, &levels);

uspace/app/wavplay/dplay.c
@@ -352,7 +352,7 @@ int dplay(const char *device, const char *file)
 		goto close_session;
 	}
 
-	const char* info = NULL;
+	char* info = NULL;
 	ret = audio_pcm_get_info_str(session, &info);
 	if (ret != EOK) {
 		printf("Failed to get PCM info: %s.\n", str_error(ret));

uspace/app/wavplay/drec.c
@@ -194,7 +194,7 @@ int drecord(const char *device, const char *file)
 		goto close_session;
 	}
 
-	const char* info = NULL;
+	char* info = NULL;
 	ret = audio_pcm_get_info_str(session, &info);
 	if (ret != EOK) {
 		printf("Failed to get PCM info.\n");

uspace/drv/bus/usb/vhc/hub/virthub.c
@@ -155,16 +155,18 @@ errno_t virthub_init(usbvirt_device_t *dev, const char* name)
 	dev->ops = &hub_ops;
 	dev->descriptors = &descriptors;
 	dev->address = 0;
-	dev->name = str_dup(name);
-	if (!dev->name)
+
+	char *n = str_dup(name);
+	if (!n)
 		return ENOMEM;
 
 	hub_t *hub = malloc(sizeof(hub_t));
 	if (hub == NULL) {
-		free(dev->name);
+		free(n);
 		return ENOMEM;
 	}
 
+	dev->name = n;
 	hub_init(hub);
 	dev->device_data = hub;
 

commit 231c770580f4400a6e0e7612723b1efb16fd5390

Fix handling of hound audio data
Commit 33b8d024bc268f2773c26e74a40dada11bb71e58 broke audio.

Hound needs to modify the buffer even after audio_data_create() returns.
The original buffer also needs to be eventually deallocated. Lastly,
the extra data copy may introduce undesired latencies.

This commit reverts the offending parts of
33b8d024bc268f2773c26e74a40dada11bb71e58.

uspace/srv/audio/hound/audio_data.c
@@ -35,7 +35,6 @@
 
 #include <macros.h>
 #include <stdlib.h>
-#include <str.h>
 
 #include "audio_data.h"
 #include "log.h"
@@ -56,8 +55,7 @@ audio_data_t *audio_data_create(const void *data, size_t size,
 		if (overflow)
 			log_warning("Data not a multiple of frame size, "
 			    "clipping.");
-		uint8_t *d = ((uint8_t *)adata) + offsetof(audio_data_t, data);
-		memcpy(d, data, size);
+		adata->data = data;
 		adata->size = size - overflow;
 		adata->format = format;
 		atomic_set(&adata->refcount, 1);
@@ -86,6 +84,7 @@ void audio_data_unref(audio_data_t *adata)
 	assert(atomic_get(&adata->refcount) > 0);
 	atomic_count_t refc = atomic_predec(&adata->refcount);
 	if (refc == 0) {
+		free((void *) adata->data);
 		free(adata);
 	}
 }

uspace/srv/audio/hound/audio_data.h
@@ -44,14 +44,14 @@
 
 /** Reference counted audio buffer */
 typedef struct {
+	/** Audio data */
+	const void *data;
 	/** Size of the buffer pointer to by data */
 	size_t size;
 	/** Format of the audio data */
 	pcm_format_t format;
 	/** Reference counter */
 	atomic_t refcount;
-	/** Audio data */
-	const uint8_t data[];
 } audio_data_t;
 
 /** Audio data pipe structure */