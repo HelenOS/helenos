6a3a64f9ca5ecfac019f3def74bb32509be36d1b
Dzejrou <dzejrou@gmail.com>
2018-05-10 02:18:17 +0200

    cpp: added owner_less

--- uspace/lib/cpp/include/internal/memory/owner_less.hpp
+++ uspace/lib/cpp/include/internal/memory/owner_less.hpp
@@ -26,9 +26,66 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <impl/memory.hpp>
-#include <internal/memory/allocator_arg.hpp>
-#include <internal/memory/addressof.hpp>
-#include <internal/memory/owner_less.hpp>
+#ifndef LIBCPP_INTERNAL_MEMORY_OWNER_LESS
+#define LIBCPP_INTERNAL_MEMORY_OWNER_LESS
+
 #include <internal/memory/shared_ptr.hpp>
 #include <internal/memory/weak_ptr.hpp>
+
+namespace std
+{
+    /**
+     * 20.8.2.4, class template owner_less:
+     */
+
+    template<class>
+    struct owner_less;
+
+    template<class T>
+    struct owner_less<shared_ptr<T>>
+    {
+        using retult_type          = bool;
+        using first_argument_type  = shared_ptr<T>;
+        using second_argument_type = shared_ptr<T>;
+
+        bool operator()(const shared_ptr<T>& lhs, const shared_ptr<T>& rhs) const
+        {
+            return lhs.owner_before(rhs);
+        }
+
+        bool operator()(const shared_ptr<T>& lhs, const weak_ptr<T>& rhs) const
+        {
+            return lhs.owner_before(rhs);
+        }
+
+        bool operator()(const weak_ptr<T>& lhs, const shared_ptr<T>& rhs) const
+        {
+            return lhs.owner_before(rhs);
+        }
+    };
+
+    template<class T>
+    struct owner_less<weak_ptr<T>>
+    {
+        using retult_type          = bool;
+        using first_argument_type  = weak_ptr<T>;
+        using second_argument_type = weak_ptr<T>;
+
+        bool operator()(const weak_ptr<T>& lhs, const weak_ptr<T>& rhs) const
+        {
+            return lhs.owner_before(rhs);
+        }
+
+        bool operator()(const shared_ptr<T>& lhs, const weak_ptr<T>& rhs) const
+        {
+            return lhs.owner_before(rhs);
+        }
+
+        bool operator()(const weak_ptr<T>& lhs, const shared_ptr<T>& rhs) const
+        {
+            return lhs.owner_before(rhs);
+        }
+    };
+}
+
+#endif
