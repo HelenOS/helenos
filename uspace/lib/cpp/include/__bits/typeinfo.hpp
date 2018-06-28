a37eeb9ba97e3e9ba5e9566f217542e2b135459b
Dzejrou <dzejrou@gmail.com>
2018-06-28 18:07:18 +0200

    cpp: fixed indentation

--- uspace/lib/cpp/include/__bits/typeinfo.hpp
+++ uspace/lib/cpp/include/__bits/typeinfo.hpp
@@ -33,27 +33,26 @@
 
 namespace std
 {
+    class type_info
+    {
+        public:
+            virtual ~type_info();
 
-class type_info
-{
-    public:
-        virtual ~type_info();
+            bool operator==(const type_info&) const noexcept;
+            bool operator!=(const type_info&) const noexcept;
 
-        bool operator==(const type_info&) const noexcept;
-        bool operator!=(const type_info&) const noexcept;
+            bool before(const type_info&) const noexcept;
 
-        bool before(const type_info&) const noexcept;
+            size_t hash_code() const noexcept;
 
-        size_t hash_code() const noexcept;
+            const char* name() const noexcept;
 
-        const char* name() const noexcept;
+            type_info(const type_info&) = delete;
+            type_info& operator=(const type_info&) = delete;
 
-        type_info(const type_info&) = delete;
-        type_info& operator=(const type_info&) = delete;
-
-    private:
-        const char* __name;
-};
+        private:
+            const char* __name;
+    };
 
     // TODO: class bad_cast, bad_typeid
 }
