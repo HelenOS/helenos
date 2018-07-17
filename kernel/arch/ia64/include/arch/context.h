58093d99e8de3e461cadfcbc02a56288a1c5a2d6
Jakub Jermar <jakub@jermar.eu>
2018-07-18 00:25:51 +0200

    ia64: context_set() must always set gp to __gp

--- kernel/arch/ia64/include/arch/context.h
+++ kernel/arch/ia64/include/arch/context.h
@@ -49,6 +49,7 @@
  */
 #define SP_DELTA  (0 + ALIGN_UP(STACK_ITEM_SIZE, STACK_ALIGNMENT))
 
+extern void *__gp;
 /* RSE stack starts at the bottom of memory stack, hence the division by 2. */
 #define context_set(c, _pc, stack, size) \
 	do { \
@@ -56,6 +57,7 @@
 		(c)->bsp = ((uintptr_t) stack) + ALIGN_UP((size / 2), REGISTER_STACK_ALIGNMENT); \
 		(c)->ar_pfs &= PFM_MASK; \
 		(c)->sp = ((uintptr_t) stack) + ALIGN_UP((size / 2), STACK_ALIGNMENT) - SP_DELTA; \
+		(c)->r1 = (uintptr_t) &__gp; \
 	} while (0)
 
 #endif
