631281dedb2b080d5b5af149d4e4b05369d66c34
Jiří Zárevúcky <jiri.zarevucky@nic.cz>
2018-09-22 16:38:04 +0200

    Work around libiconv stupidity by not including <offset.h> from <sys/types.h>

--- uspace/lib/posix/include/posix/sys/types.h
+++ uspace/lib/posix/include/posix/sys/types.h
@@ -36,14 +36,13 @@
 #ifndef POSIX_SYS_TYPES_H_
 #define POSIX_SYS_TYPES_H_
 
-#include <offset.h>
 #include <types/common.h>
 
 typedef unsigned int ino_t;
 typedef unsigned int nlink_t;
 typedef unsigned int uid_t;
 typedef unsigned int gid_t;
-typedef off64_t off_t;
+typedef int64_t off_t;
 typedef long blksize_t;
 typedef long blkcnt_t;
 typedef int pid_t;
