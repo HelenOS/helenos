9df0f64255a1e09efd87f6b2371e99e87f9e8350
Jakub Jermar <jakub@jermar.eu>
2018-11-22 21:05:37 +0100

    Fix function reference counting
    After commit 498ced1, a function is created with an implicit reference.
    Adding an extra reference for creation thus adds a reference that will
    never be dropped and the function will be leaked.
    
    This commit removes the extra reference.

--- uspace/lib/drv/generic/driver.c
+++ uspace/lib/drv/generic/driver.c
@@ -730,9 +730,8 @@ ddf_fun_t *ddf_fun_create(ddf_dev_t *dev, fun_type_t ftype, const char *name)
 	if (fun == NULL)
 		return NULL;
 
-	/* Add one reference that will be dropped by ddf_fun_destroy() */
 	fun->dev = dev;
-	fun_add_ref(fun);
+	dev_add_ref(fun->dev);
 
 	fun->bound = false;
 	fun->ftype = ftype;
@@ -740,7 +739,7 @@ ddf_fun_t *ddf_fun_create(ddf_dev_t *dev, fun_type_t ftype, const char *name)
 	if (name != NULL) {
 		fun->name = str_dup(name);
 		if (fun->name == NULL) {
-			delete_function(fun);
+			fun_del_ref(fun);	/* fun is destroyed */
 			return NULL;
 		}
 	}
