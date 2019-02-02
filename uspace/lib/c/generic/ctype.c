67ca359bec203b53588e6ce170a1340edd4a904f
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2019-02-02 18:05:56 +0100

    Functions in <ctype.h> should be external
    Standard says so.

--- uspace/lib/c/generic/ctype.c
+++ uspace/lib/c/generic/ctype.c
@@ -26,22 +26,102 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef LIBC_CTYPE_H_
-#define LIBC_CTYPE_H_
+/** @addtogroup libc
+ * @{
+ */
+/** @file
+ */
 
-int islower(int);
-int isupper(int);
-int isalpha(int);
-int isdigit(int);
-int isalnum(int);
-int isblank(int);
-int iscntrl(int);
-int isprint(int);
-int isgraph(int);
-int isspace(int);
-int ispunct(int);
-int isxdigit(int);
-int tolower(int);
-int toupper(int);
+#include <ctype.h>
 
-#endif
+int islower(int c)
+{
+	return ((c >= 'a') && (c <= 'z'));
+}
+
+int isupper(int c)
+{
+	return ((c >= 'A') && (c <= 'Z'));
+}
+
+int isalpha(int c)
+{
+	return (islower(c) || isupper(c));
+}
+
+int isdigit(int c)
+{
+	return ((c >= '0') && (c <= '9'));
+}
+
+int isalnum(int c)
+{
+	return (isalpha(c) || isdigit(c));
+}
+
+int isblank(int c)
+{
+	return c == ' ' || c == '\t';
+}
+
+int iscntrl(int c)
+{
+	return (c >= 0 && c < 0x20) || c == 0x7E;
+}
+
+int isprint(int c)
+{
+	return c >= 0 && c < 0x80 && !iscntrl(c);
+}
+
+int isgraph(int c)
+{
+	return isprint(c) && c != ' ';
+}
+
+int isspace(int c)
+{
+	switch (c) {
+	case ' ':
+	case '\n':
+	case '\t':
+	case '\f':
+	case '\r':
+	case '\v':
+		return 1;
+		break;
+	default:
+		return 0;
+	}
+}
+
+int ispunct(int c)
+{
+	return !isspace(c) && !isalnum(c) && isprint(c);
+}
+
+int isxdigit(int c)
+{
+	return isdigit(c) ||
+	    (c >= 'a' && c <= 'f') ||
+	    (c >= 'A' && c <= 'F');
+}
+
+int tolower(int c)
+{
+	if (isupper(c))
+		return (c + ('a' - 'A'));
+	else
+		return c;
+}
+
+int toupper(int c)
+{
+	if (islower(c))
+		return (c + ('A' - 'a'));
+	else
+		return c;
+}
+
+/** @}
+ */
