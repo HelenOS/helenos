0d0f1a89993d988c451b6f2d8a417d6777b4632a
Matthieu Riolo <matthieu.riolo@googlemail.com>
2019-06-22 18:06:39 +0200

    Unalias prints out warning when an alias could not be found but proceeds to delete the other given aliases. Also the flag -a will be treated differentially. In the old version the flag could have been given with other alias names for deletion. The new version will now separate those two cases. Either the flag -a is given or names for deletion are given.

--- uspace/app/bdsh/cmds/modules/unalias/unalias.c
+++ uspace/app/bdsh/cmds/modules/unalias/unalias.c
@@ -57,11 +57,12 @@ void help_cmd_unalias(unsigned int level)
 		printf("`%s' removes an alias or all aliases with -a\n", cmdname);
 	} else {
 		help_cmd_unalias(HELP_SHORT);
-		printf("Usage: `%s' [-a] name [name ...]'\n\n"
+		printf("Usage: `%s' -a'\n"
+		    "`%s' name [name ...]'\n\n"
 		    "If no parameters are given it will display this help message.\n"
 		    "If the flag -a is given, all existing aliases will be removed.\n"
 		    "If one or multiple parameters are given, then those aliases will be removed.\n",
-		    cmdname);
+		    cmdname, cmdname);
 	}
 }
 
@@ -74,11 +75,11 @@ int cmd_unalias(char **argv)
 		return CMD_SUCCESS;
 	}
 
-	size_t i;
 	odlink_t *alias_link;
 
-	for (i = 1; argv[i] != NULL; i++) {
-		if (str_cmp(argv[i], "-a") == 0) {
+	unsigned int argc = cli_count_args(argv);
+	if (argc == 2) {
+		if (str_cmp(argv[1], "-a") == 0) {
 			alias_link = odict_first(&alias_dict);
 			while (alias_link != NULL) {
 				odlink_t *old_alias_link = alias_link;
@@ -88,15 +89,17 @@ int cmd_unalias(char **argv)
 
 			return CMD_SUCCESS;
 		}
+	}
 
+	size_t i;
+	for (i = 1; argv[i] != NULL; i++) {
 		alias_link = odict_find_eq(&alias_dict, (void *)argv[i], NULL);
 
 		if (alias_link == NULL) {
 			cli_error(CL_ENOENT, "%s: No alias '%s' found\n", cmdname, argv[i]);
-			return CMD_FAILURE;
+		} else {
+			free_alias(alias_link);
 		}
-
-		free_alias(alias_link);
 	}
 
 	return CMD_SUCCESS;
