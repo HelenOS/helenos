8684e346eaa5bac88522301324eb2cebb94b52a0
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2019-06-29 14:38:43 +0200

    ppc32 boot

---
+++ boot/yaboot/build_dist.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+BOOT_OUTPUT="$1"
+SRCDIR="$2"
+DESTDIR="$3"
+
+rm -rf ${DESTDIR}
+
+mkdir -p ${DESTDIR}/boot
+cp -t ${DESTDIR}/boot \
+	${SRCDIR}/ofboot.b \
+	${SRCDIR}/yaboot \
+	${SRCDIR}/yaboot.conf
+cp ${BOOT_OUTPUT} ${DESTDIR}/boot/image.boot
+
+mkdir -p ${DESTDIR}/ppc
+cp -t ${DESTDIR}/ppc \
+	${SRCDIR}/bootinfo.txt
