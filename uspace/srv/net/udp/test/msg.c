46a8c3cd2588984cf4d1b909ed12113e2f780298
Jiri Svoboda <jiri@wiwaxia>
2019-10-14 19:52:30 +0200

    Add unit tests for UDP servers' msg and pdu modules

--- uspace/srv/net/udp/test/msg.c
+++ uspace/srv/net/udp/test/msg.c
@@ -27,11 +27,34 @@
  */
 
 #include <pcut/pcut.h>
+#include <str.h>
+#include "../msg.h"
 
 PCUT_INIT;
 
-PCUT_IMPORT(assoc);
-PCUT_IMPORT(msg);
-PCUT_IMPORT(pdu);
+PCUT_TEST_SUITE(msg);
 
-PCUT_MAIN();
+/** Test creating and deleting dummy message */
+PCUT_TEST(new_delete)
+{
+	udp_msg_t *msg;
+
+	msg = udp_msg_new();
+	udp_msg_delete(msg);
+}
+
+/** Test creating, filling in and deleting message */
+PCUT_TEST(new_fill_in_delete)
+{
+	udp_msg_t *msg;
+	const char *msgstr = "Hello";
+
+	msg = udp_msg_new();
+	PCUT_ASSERT_NOT_NULL(msg);
+	msg->data_size = str_size(msgstr) + 1;
+	msg->data = str_dup(msgstr);
+
+	udp_msg_delete(msg);
+}
+
+PCUT_EXPORT(msg);
