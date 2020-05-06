0800b26e02a1ef91aef8c088fe007d3c72dbc797
Jiri Svoboda <jiri@wiwaxia>
2020-05-06 12:30:30 +0200

    Do not leak exchanges in udebug client API

--- uspace/lib/c/generic/udebug.c
+++ uspace/lib/c/generic/udebug.c
@@ -41,19 +41,28 @@
 errno_t udebug_begin(async_sess_t *sess)
 {
 	async_exch_t *exch = async_exchange_begin(sess);
-	return async_req_1_0(exch, IPC_M_DEBUG, UDEBUG_M_BEGIN);
+	errno_t rc = async_req_1_0(exch, IPC_M_DEBUG, UDEBUG_M_BEGIN);
+	async_exchange_end(exch);
+
+	return rc;
 }
 
 errno_t udebug_end(async_sess_t *sess)
 {
 	async_exch_t *exch = async_exchange_begin(sess);
-	return async_req_1_0(exch, IPC_M_DEBUG, UDEBUG_M_END);
+	errno_t rc = async_req_1_0(exch, IPC_M_DEBUG, UDEBUG_M_END);
+	async_exchange_end(exch);
+
+	return rc;
 }
 
 errno_t udebug_set_evmask(async_sess_t *sess, udebug_evmask_t mask)
 {
 	async_exch_t *exch = async_exchange_begin(sess);
-	return async_req_2_0(exch, IPC_M_DEBUG, UDEBUG_M_SET_EVMASK, mask);
+	errno_t rc = async_req_2_0(exch, IPC_M_DEBUG, UDEBUG_M_SET_EVMASK, mask);
+	async_exchange_end(exch);
+
+	return rc;
 }
 
 errno_t udebug_thread_read(async_sess_t *sess, void *buffer, size_t n,
@@ -64,6 +73,7 @@ errno_t udebug_thread_read(async_sess_t *sess, void *buffer, size_t n,
 	async_exch_t *exch = async_exchange_begin(sess);
 	errno_t rc = async_req_3_3(exch, IPC_M_DEBUG, UDEBUG_M_THREAD_READ,
 	    (sysarg_t) buffer, n, NULL, &a_copied, &a_needed);
+	async_exchange_end(exch);
 
 	*copied = (size_t) a_copied;
 	*needed = (size_t) a_needed;
@@ -79,6 +89,7 @@ errno_t udebug_name_read(async_sess_t *sess, void *buffer, size_t n,
 	async_exch_t *exch = async_exchange_begin(sess);
 	errno_t rc = async_req_3_3(exch, IPC_M_DEBUG, UDEBUG_M_NAME_READ,
 	    (sysarg_t) buffer, n, NULL, &a_copied, &a_needed);
+	async_exchange_end(exch);
 
 	*copied = (size_t) a_copied;
 	*needed = (size_t) a_needed;
@@ -94,6 +105,7 @@ errno_t udebug_areas_read(async_sess_t *sess, void *buffer, size_t n,
 	async_exch_t *exch = async_exchange_begin(sess);
 	errno_t rc = async_req_3_3(exch, IPC_M_DEBUG, UDEBUG_M_AREAS_READ,
 	    (sysarg_t) buffer, n, NULL, &a_copied, &a_needed);
+	async_exchange_end(exch);
 
 	*copied = (size_t) a_copied;
 	*needed = (size_t) a_needed;
@@ -104,22 +116,31 @@ errno_t udebug_areas_read(async_sess_t *sess, void *buffer, size_t n,
 errno_t udebug_mem_read(async_sess_t *sess, void *buffer, uintptr_t addr, size_t n)
 {
 	async_exch_t *exch = async_exchange_begin(sess);
-	return async_req_4_0(exch, IPC_M_DEBUG, UDEBUG_M_MEM_READ,
+	errno_t rc = async_req_4_0(exch, IPC_M_DEBUG, UDEBUG_M_MEM_READ,
 	    (sysarg_t) buffer, addr, n);
+	async_exchange_end(exch);
+
+	return rc;
 }
 
 errno_t udebug_args_read(async_sess_t *sess, thash_t tid, sysarg_t *buffer)
 {
 	async_exch_t *exch = async_exchange_begin(sess);
-	return async_req_3_0(exch, IPC_M_DEBUG, UDEBUG_M_ARGS_READ,
+	errno_t rc = async_req_3_0(exch, IPC_M_DEBUG, UDEBUG_M_ARGS_READ,
 	    tid, (sysarg_t) buffer);
+	async_exchange_end(exch);
+
+	return rc;
 }
 
 errno_t udebug_regs_read(async_sess_t *sess, thash_t tid, void *buffer)
 {
 	async_exch_t *exch = async_exchange_begin(sess);
-	return async_req_3_0(exch, IPC_M_DEBUG, UDEBUG_M_REGS_READ,
+	errno_t rc = async_req_3_0(exch, IPC_M_DEBUG, UDEBUG_M_REGS_READ,
 	    tid, (sysarg_t) buffer);
+	async_exchange_end(exch);
+
+	return rc;
 }
 
 errno_t udebug_go(async_sess_t *sess, thash_t tid, udebug_event_t *ev_type,
@@ -130,6 +151,7 @@ errno_t udebug_go(async_sess_t *sess, thash_t tid, udebug_event_t *ev_type,
 	async_exch_t *exch = async_exchange_begin(sess);
 	errno_t rc = async_req_2_3(exch, IPC_M_DEBUG, UDEBUG_M_GO,
 	    tid, &a_ev_type, val0, val1);
+	async_exchange_end(exch);
 
 	*ev_type = a_ev_type;
 	return rc;
@@ -138,7 +160,10 @@ errno_t udebug_go(async_sess_t *sess, thash_t tid, udebug_event_t *ev_type,
 errno_t udebug_stop(async_sess_t *sess, thash_t tid)
 {
 	async_exch_t *exch = async_exchange_begin(sess);
-	return async_req_2_0(exch, IPC_M_DEBUG, UDEBUG_M_STOP, tid);
+	errno_t rc = async_req_2_0(exch, IPC_M_DEBUG, UDEBUG_M_STOP, tid);
+	async_exchange_end(exch);
+
+	return rc;
 }
 
 /** @}
