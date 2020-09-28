efe08810ecfe4be5754fd5b8c53803baa3bb6392
Jiri Svoboda <jiri@wiwaxia>
2020-09-28 22:56:48 +0200

    Fix 32-bit build

--- uspace/lib/riff/src/chunk.c
+++ uspace/lib/riff/src/chunk.c
@@ -422,8 +422,10 @@ errno_t riff_rchunk_seek(riff_rchunk_t *rchunk, long offset, int whence)
 		return EINVAL;
 	}
 
-	if (dest < rchunk->ckstart || dest > rchunk->ckstart + rchunk->cksize)
+	if (dest < rchunk->ckstart || (unsigned long) dest > rchunk->ckstart +
+	    rchunk->cksize) {
 		return ELIMIT;
+	}
 
 	rv = fseek(rchunk->riffr->f, dest, SEEK_SET);
 	if (rv < 0)
