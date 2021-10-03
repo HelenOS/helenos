5d466a1efeb2e4d32896d94a1e77a6e562b461aa
Jiri Svoboda <jiri@wiwaxia>
2021-10-03 19:33:47 +0200

    Add menu, make window full-screen

---
+++ uspace/app/nav/menu.c
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2021 Jiri Svoboda
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @addtogroup nav
+ * @{
+ */
+/** @file Navigator.
+ *
+ * HelenOS file manager.
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <ui/menu.h>
+#include <ui/menubar.h>
+#include <ui/menuentry.h>
+#include "menu.h"
+#include "nav.h"
+
+static void nav_file_exit(ui_menu_entry_t *, void *);
+
+/** Create navigator menu.
+ *
+ * @param navigator Navigator
+ * @param rmenu Place to store pointer to new menu
+ * @return EOK on success or an error code
+ */
+errno_t nav_menu_create(navigator_t *navigator, nav_menu_t **rmenu)
+{
+	nav_menu_t *menu;
+	ui_menu_t *mfile;
+	ui_menu_entry_t *mexit;
+	gfx_rect_t arect;
+	gfx_rect_t rect;
+	errno_t rc;
+
+	menu = calloc(1, sizeof(nav_menu_t));
+	if (menu == NULL)
+		return ENOMEM;
+
+	rc = ui_menu_bar_create(navigator->ui, navigator->window,
+	    &menu->menubar);
+	if (rc != EOK)
+		goto error;
+
+	rc = ui_menu_create(menu->menubar, "File", &mfile);
+	if (rc != EOK)
+		goto error;
+
+	rc = ui_menu_entry_create(mfile, "Exit", "Ctrl-Q", &mexit);
+	if (rc != EOK)
+		goto error;
+
+	ui_menu_entry_set_cb(mexit, nav_file_exit, (void *) navigator);
+
+	ui_window_get_app_rect(navigator->window, &arect);
+
+	rect.p0 = arect.p0;
+	rect.p1.x = arect.p1.x;
+	rect.p1.y = arect.p0.y + 1;
+	ui_menu_bar_set_rect(menu->menubar, &rect);
+
+	rc = ui_fixed_add(navigator->fixed, ui_menu_bar_ctl(menu->menubar));
+	if (rc != EOK)
+		goto error;
+
+	menu->navigator = navigator;
+	*rmenu = menu;
+	return EOK;
+error:
+	nav_menu_destroy(menu);
+	return rc;
+}
+
+/** Destroy navigator menu.
+ *
+ * @param menu Menu
+ */
+void nav_menu_destroy(nav_menu_t *menu)
+{
+	if (menu->menubar != NULL) {
+		ui_fixed_remove(menu->navigator->fixed,
+		    ui_menu_bar_ctl(menu->menubar));
+		ui_menu_bar_destroy(menu->menubar);
+	}
+
+	free(menu);
+}
+
+/** File / Exit menu entry selected.
+ *
+ * @param mentry Menu entry
+ * @param arg Argument (navigator_t *)
+ */
+static void nav_file_exit(ui_menu_entry_t *mentry, void *arg)
+{
+	navigator_t *navigator = (navigator_t *) arg;
+
+	ui_quit(navigator->ui);
+}
+
+/** @}
+ */
