f1bed8578a7fdd1d05d754ebd83b76b77de1720a
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2022-08-13 14:43:19 +0200

    Update sysel scripts

--- tools/transform-copyright.sh
+++ tools/transform-copyright.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-# git grep -l --null "^# Copyright\b" -- . | xargs -r -L1 -0 tools/transform-copyright.sh
+# git grep -l --null "^-- Copyright\b" -- . | xargs -r -L1 -0 tools/transform-copyright.sh
 
 FILENAME="$1"
 
-grep -F '# Copyright' $FILENAME > $FILENAME.copyright__
-grep -v -F '# Copyright' $FILENAME > $FILENAME.nocopyright__
-head -n 24 $FILENAME.nocopyright__ > $FILENAME.license__
+grep -F -e '-- Copyright' $FILENAME > $FILENAME.copyright__
+grep -v -F -e '-- Copyright' $FILENAME > $FILENAME.nocopyright__
+head -n 26 $FILENAME.nocopyright__ > $FILENAME.license__
 
 if diff -q $FILENAME.license__ license_text_tmp.txt; then
-	tail -n +25 $FILENAME.nocopyright__ > $FILENAME.nolicense__
-	echo '#!/usr/bin/perl -w' > $FILENAME
-	echo "#" >> $FILENAME
+	tail -n +27 $FILENAME.nocopyright__ > $FILENAME.nolicense__
+	echo "--" > $FILENAME
 	sed 's/Copyright (c)/SPDX-FileCopyrightText:/g' $FILENAME.copyright__ >> $FILENAME
-	echo "#" >> $FILENAME
-	echo "# SPDX-License-Identifier: BSD-3-Clause" >> $FILENAME
-	echo "#" >> $FILENAME
+	echo "--" >> $FILENAME
+	echo "-- SPDX-License-Identifier: BSD-3-Clause" >> $FILENAME
+	echo "--" >> $FILENAME
 	
 	cat $FILENAME.nolicense__ >> $FILENAME
 
