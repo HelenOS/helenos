HelenOS/helenos

commit 4777e022eec9ed5750ce62ae94a262f006a515fc

Make spinlock functions inlineable in non-SMP case
By popular demand.

kernel/generic/include/synch/spinlock.h
@@ -116,11 +116,43 @@ typedef struct spinlock {
 #define SPINLOCK_STATIC_INITIALIZE(lock_name) \
 	SPINLOCK_STATIC_INITIALIZE_NAME(lock_name, #lock_name)
 
+#ifdef CONFIG_SMP
+
 extern void spinlock_initialize(spinlock_t *, const char *);
 extern bool spinlock_trylock(spinlock_t *);
 extern void spinlock_lock(spinlock_t *);
 extern void spinlock_unlock(spinlock_t *);
 extern bool spinlock_locked(spinlock_t *);
+
+#else
+
+#include <preemption.h>
+
+static inline void spinlock_initialize(spinlock_t *l, const char *name)
+{
+}
+
+static inline bool spinlock_trylock(spinlock_t *l)
+{
+	return true;
+}
+
+static inline void spinlock_lock(spinlock_t *l)
+{
+	preemption_disable();
+}
+
+static inline void spinlock_unlock(spinlock_t *l)
+{
+	preemption_enable();
+}
+
+static inline bool spinlock_locked(spinlock_t *l)
+{
+	return true;
+}
+
+#endif
 
 typedef struct {
 	spinlock_t lock;              /**< Spinlock */

kernel/generic/src/synch/spinlock.c
@@ -36,6 +36,8 @@
  * @brief Spinlocks.
  */
 
+#ifdef CONFIG_SMP
+
 #include <arch/asm.h>
 #include <synch/spinlock.h>
 #include <atomic.h>
@@ -55,11 +57,9 @@
  */
 void spinlock_initialize(spinlock_t *lock, const char *name)
 {
-#ifdef CONFIG_SMP
 	atomic_flag_clear_explicit(&lock->flag, memory_order_relaxed);
 #ifdef CONFIG_DEBUG_SPINLOCK
 	lock->name = name;
-#endif
 #endif
 }
 
@@ -72,7 +72,6 @@ void spinlock_lock(spinlock_t *lock)
 {
 	preemption_disable();
 
-#ifdef CONFIG_SMP
 	bool deadlock_reported = false;
 	size_t i = 0;
 
@@ -119,8 +118,6 @@ void spinlock_lock(spinlock_t *lock)
 
 	if (deadlock_reported)
 		printf("cpu%u: not deadlocked\n", CPU->id);
-
-#endif
 }
 
 /** Unlock spinlock
@@ -129,14 +126,11 @@ void spinlock_lock(spinlock_t *lock)
  */
 void spinlock_unlock(spinlock_t *lock)
 {
-#ifdef CONFIG_SMP
 #ifdef CONFIG_DEBUG_SPINLOCK
 	ASSERT_SPINLOCK(spinlock_locked(lock), lock);
 #endif
 
 	atomic_flag_clear_explicit(&lock->flag, memory_order_release);
-#endif
-
 	preemption_enable();
 }
 
@@ -153,16 +147,12 @@ bool spinlock_trylock(spinlock_t *lock)
 {
 	preemption_disable();
 
-#ifdef CONFIG_SMP
 	bool ret = !atomic_flag_test_and_set_explicit(&lock->flag, memory_order_acquire);
 
 	if (!ret)
 		preemption_enable();
 
 	return ret;
-#else
-	return true;
-#endif
 }
 
 /** Find out whether the spinlock is currently locked.
@@ -172,7 +162,6 @@ bool spinlock_trylock(spinlock_t *lock)
  */
 bool spinlock_locked(spinlock_t *lock)
 {
-#ifdef CONFIG_SMP
 	// NOTE: Atomic flag doesn't support simple atomic read (by design),
 	//       so instead we test_and_set and then clear if necessary.
 	//       This function is only used inside assert, so we don't need
@@ -182,10 +171,9 @@ bool spinlock_locked(spinlock_t *lock)
 	if (!ret)
 		atomic_flag_clear_explicit(&lock->flag, memory_order_relaxed);
 	return ret;
-#else
-	return true;
-#endif
 }
+
+#endif  /* CONFIG_SMP */
 
 /** @}
  */

commit 4f84ee4268fbebe449f1346536fa63cef10bd266

Fix a bug accidentally introduced in 4777e022

kernel/generic/include/synch/spinlock.h
@@ -134,6 +134,7 @@ static inline void spinlock_initialize(spinlock_t *l, const char *name)
 
 static inline bool spinlock_trylock(spinlock_t *l)
 {
+	preemption_disable();
 	return true;
 }
 