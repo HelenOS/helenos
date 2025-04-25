00eeffa5aa242b2af8d46a65e8f13cff1d01eb89
Matěj Volf <git@mvolfik.com>
2025-04-25 17:41:22 +0200

    revert to major.minor versions, determine unversioned name automatically

--- meson/part/exports/copy-export.sh
+++ meson/part/exports/copy-export.sh
@@ -46,8 +46,12 @@ while [ "$#" -gt 0 ]; do
             ;;
         sharedlib)
             filename="$(basename "$2")"
+            major_versioned_name="$3"
+            # strip the last `.123` part
+            unversioned_name="$(echo "$major_versioned_name" | rev | cut -d. -f2- | rev)"
             cp -L "$2" "${target_dir}/lib/$filename"
-            ln -s "$filename" "${target_dir}/lib/$3"
+            ln -s "$filename" "${target_dir}/lib/$major_versioned_name"
+            ln -s "$filename" "${target_dir}/lib/$unversioned_name"
             ;;
         include)
             mkdir -p "${target_dir}/include/$3"
